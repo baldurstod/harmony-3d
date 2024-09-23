d45395d0b538fb731f7afb857fae9dba5c4f6379
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-09-23 23:38:55 +0200

    Added JSONLoader

---
+++ src/loaders/jsonloader.ts
@@ -0,0 +1,43 @@
+import { getEntity, registerEntity } from '../entities/entities';
+import { Entity } from '../entities/entity';
+import { Material } from '../materials/material';
+
+export class JSONLoader {
+	static async fromJSON(rootEntity: object) {
+		let loadedResolve: Function = () => { };// Note: typescript falsely complains about loadedResolve not being assigned without this.
+		const loadedPromise = new Promise<void>(resolve => {
+			loadedResolve = resolve;
+		});
+		const entities = new Map<string, Entity | Material>();
+		const root = await this.loadEntity(rootEntity, entities, loadedPromise);
+		loadedResolve(true);
+		return root;
+	}
+
+	static async loadEntity(jsonEntity, entities: Map<string, Entity | Material>, loadedPromise: Promise<void>) {
+		if (jsonEntity) {
+			const constructor = getEntity(jsonEntity.constructor);
+			if (constructor) {
+				let entity = await constructor.constructFromJSON(jsonEntity, entities, loadedPromise);
+				entity.fromJSON(jsonEntity);
+				entities.set(entity.id, entity);
+
+				if (jsonEntity.children) {
+					for (let child of jsonEntity.children) {
+						let childEntity = await this.loadEntity(child, entities, loadedPromise);
+						if (childEntity && entity['addChild']) {
+							entity['addChild'](childEntity);
+						}
+					}
+				}
+				return entity;
+			} else {
+				console.error('Unknown constructor', jsonEntity.constructor);
+			}
+		}
+	}
+
+	static registerEntity(ent: typeof Entity | typeof Material) {
+		registerEntity(ent);
+	}
+}
