4c73842e2a60cc4dce40a6879f1eb1fc7f3eed34
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-09-23 23:42:35 +0200

    Added MateriaParameter

---
+++ src/materials/materialparameter.ts
@@ -0,0 +1,68 @@
+import { isVec4 } from '../math/vector';
+
+export enum MateriaParameterType {
+	None = 0,
+	Boolean,
+	Integer,
+	Float,
+	NormalizedFloat,
+	ClampedFloat,
+	Vec2,
+	Vec3,
+	Vec4,
+	Mat2,
+	Mat3,
+	Mat4,
+	Color2,
+	Color3,
+	Color4,
+	Texture,
+	Texture1D,
+	Texture2D,
+	Texture3D,
+}
+
+export type ParameterChanged = (newValue: any, oldValue?: any) => void;
+
+export class MateriaParameter {
+	#name: string;
+	#type: MateriaParameterType = MateriaParameterType.None;
+	#value: any
+	#changed?: ParameterChanged
+	constructor(name: string, type: MateriaParameterType, value: any, changed?: ParameterChanged) {
+		this.#name = name;
+		this.#type = type;
+		this.#changed = changed;
+		this.setValue(value);
+	}
+
+	setValue(value: any) {
+		if (!this.#checkValue(value)) {
+			console.warn('Material parameter value has an incorrect type');
+		}
+		// Todo: check value type
+		// Todo: check if value actually changed
+		if (this.#changed) {
+			this.#changed(value, this.#value);
+		}
+		this.#value = value;
+	}
+
+	#checkValue(value: any): boolean {
+		if (value === undefined || value === null) {
+			return true;
+		}
+
+		switch (this.#type) {
+			case MateriaParameterType.NormalizedFloat:
+				return (typeof value == 'number') && (value >= 0) && (value <= 1);
+			case MateriaParameterType.Color4:
+				return isVec4(value);
+			case MateriaParameterType.Texture:
+				return value.isTexture;
+			default:
+				throw 'unknown type: ' + this.#type;
+				break;
+		}
+	}
+}
