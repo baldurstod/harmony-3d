c48ec34c0bcc81df971aff36f2483a560287611b
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-09-30 14:29:36 +0200

    Added Box, BoxBufferGeometry

---
+++ src/primitives/box.ts
@@ -0,0 +1,72 @@
+import { BoxBufferGeometry } from './geometries/boxbuffergeometry';
+import { JSONLoader } from '../loaders/jsonloader';
+import { MeshBasicMaterial } from '../materials/meshbasicmaterial';
+import { Mesh } from '../objects/mesh';
+
+export class Box extends Mesh {
+	#widthSegments: number;
+	#heightSegments: number;
+	#depthSegments: number;
+	#width: number;
+	#height: number;
+	#depth: number;
+	constructor({ width = 1, height = undefined, depth = undefined, material = new MeshBasicMaterial(), widthSegments = 1, heightSegments = 1, depthSegments = 1 } = {}) {
+		super(new BoxBufferGeometry(), material);
+		this.#width = width ?? 1;
+		this.#height = height ?? this.#width;
+		this.#depth = depth ?? this.#width;
+		this.#widthSegments = widthSegments ?? 1;
+		this.#heightSegments = heightSegments ?? 1;
+		this.#depthSegments = depthSegments ?? 1;
+		this.setMaterial(material ?? new MeshBasicMaterial());
+
+		this.setGeometry(new BoxBufferGeometry());
+		this.updateGeometry();
+	}
+
+	updateGeometry() {
+		(this.geometry as BoxBufferGeometry).updateGeometry(this.#width, this.#height, this.#depth, this.#widthSegments, this.#heightSegments, this.#depthSegments);
+	}
+
+	buildContextMenu() {
+		return Object.assign(super.buildContextMenu(), {
+			Box_1: null,
+			width: { i18n: '#width', f: () => { let width = prompt('Width', String(this.#width)); if (width) { this.#width = Number(width); this.updateGeometry(); } } },
+			height: { i18n: '#height', f: () => { let height = prompt('Height', String(this.#height)); if (height) { this.#height = Number(height); this.updateGeometry(); } } },
+			depth: { i18n: '#depth', f: () => { let depth = prompt('Depth', String(this.#depth)); if (depth) { this.#depth = Number(depth); this.updateGeometry(); } } },
+			cube: { i18n: '#cube', f: () => { let size: string | number = prompt('Cube size', '0'); if (size) { size = Number(size); this.#width = size; this.#height = size; this.#depth = size; this.updateGeometry(); } } },
+		});
+	}
+
+	toJSON() {
+		let json = super.toJSON();
+		json.width = this.#width;
+		json.height = this.#height;
+		json.depth = this.#depth;
+		json.widthSegments = this.#widthSegments;
+		json.heightSegments = this.#heightSegments;
+		json.depthSegments = this.#depthSegments;
+		json.material = this.material.toJSON();
+		return json;
+	}
+
+	static async constructFromJSON(json, entities, loadedPromise) {
+		let material = await JSONLoader.loadEntity(json.material, entities, loadedPromise);
+		return new Box({ width: json.width, height: json.height, depth: json.depth, material: material, widthSegments: json.widthSegments, heightSegments: json.heightSegments, depthSegments: json.depthSegments });
+	}
+
+	/*dispose() {
+		super.dispose();
+		this.geometry.dispose();
+		this.material.dispose();
+	}*/
+
+	get entityName() {
+		return 'Box';
+	}
+
+	static get entityName() {
+		return 'Box';
+	}
+}
+JSONLoader.registerEntity(Box);
---
+++ src/primitives/geometries/boxbuffergeometry.ts
@@ -0,0 +1,126 @@
+import { vec3 } from 'gl-matrix';
+
+import { Float32BufferAttribute, Uint16BufferAttribute } from '../../geometry/bufferattribute.js'
+import { BufferGeometry } from '../../geometry/buffergeometry.js';
+
+export class BoxBufferGeometry extends BufferGeometry {
+	#indices: number[];
+	#vertices: number[];
+	#normals: number[];
+	#uvs: number[];
+	#numberOfVertices: number;
+
+	updateGeometry(width?: number, height?: number, depth?: number, widthSegments?: number, heightSegments?: number, depthSegments?: number) {
+		widthSegments = Math.floor(widthSegments);
+		heightSegments = Math.floor(heightSegments);
+		depthSegments = Math.floor(depthSegments);
+
+		// buffers
+		this.#indices = [];
+		this.#vertices = [];
+		this.#normals = [];
+		this.#uvs = [];
+
+		// helper variables
+		this.#numberOfVertices = 0;
+
+		// build each side of the box geometry
+		this.#buildPlane(2, 1, 0, - 1, - 1, depth, height, width, depthSegments, heightSegments); // px
+		this.#buildPlane(2, 1, 0, 1, - 1, depth, height, - width, depthSegments, heightSegments); // nx
+		this.#buildPlane(0, 2, 1, 1, 1, width, depth, height, widthSegments, depthSegments); // py
+		this.#buildPlane(0, 2, 1, 1, - 1, width, depth, - height, widthSegments, depthSegments); // ny
+		this.#buildPlane(0, 1, 2, 1, - 1, width, height, depth, widthSegments, heightSegments); // pz
+		this.#buildPlane(0, 1, 2, - 1, - 1, width, height, - depth, widthSegments, heightSegments); // nz
+
+		// build geometry
+		this.setIndex(new Uint16BufferAttribute(this.#indices, 1));
+		this.setAttribute('aVertexPosition', new Float32BufferAttribute(this.#vertices, 3));
+		this.setAttribute('aVertexNormal', new Float32BufferAttribute(this.#normals, 3));
+		this.setAttribute('aTextureCoord', new Float32BufferAttribute(this.#uvs, 2));
+		this.count = this.#indices.length;
+	}
+
+	#buildPlane(u, v, w, udir, vdir, width, height, depth, gridX, gridY) {
+		var segmentWidth = width / gridX;
+		var segmentHeight = height / gridY;
+
+		var widthHalf = width / 2;
+		var heightHalf = height / 2;
+		var depthHalf = depth / 2;
+
+		var gridX1 = gridX + 1;
+		var gridY1 = gridY + 1;
+
+		var vertexCounter = 0;
+
+		var ix, iy;
+
+		var vector = vec3.create();
+
+		// generate vertices, normals and uvs
+
+		for (iy = 0; iy < gridY1; iy++) {
+			var y = iy * segmentHeight - heightHalf;
+
+			for (ix = 0; ix < gridX1; ix++) {
+
+				var x = ix * segmentWidth - widthHalf;
+
+				// set values to correct vector component
+
+				vector[u] = x * udir;
+				vector[v] = y * vdir;
+				vector[w] = depthHalf;
+
+				// now apply vector to vertex buffer
+
+				this.#vertices.push(...vector);
+
+				// set values to correct vector component
+
+				vector[u] = 0;
+				vector[v] = 0;
+				vector[w] = depth > 0 ? 1 : - 1;
+
+				// now apply vector to normal buffer
+
+				this.#normals.push(...vector);
+
+				// uvs
+
+				this.#uvs.push(ix / gridX);
+				this.#uvs.push(1 - (iy / gridY));
+
+				// counters
+
+				vertexCounter += 1;
+			}
+		}
+
+		// indices
+
+		// 1. you need three indices to draw a single face
+		// 2. a single segment consists of two faces
+		// 3. so we need to generate six (2*3) indices per segment
+		for (iy = 0; iy < gridY; iy++) {
+			for (ix = 0; ix < gridX; ix++) {
+				var a = this.#numberOfVertices + ix + gridX1 * iy;
+				var b = this.#numberOfVertices + ix + gridX1 * (iy + 1);
+				var c = this.#numberOfVertices + (ix + 1) + gridX1 * (iy + 1);
+				var d = this.#numberOfVertices + (ix + 1) + gridX1 * iy;
+
+				// faces
+				this.#indices.push(a, b, d);
+				this.#indices.push(b, c, d);
+			}
+		}
+
+		// add a group to the geometry. this will ensure multi material support
+
+		// calculate new start value for groups
+
+		// update total number of vertices
+
+		this.#numberOfVertices += vertexCounter;
+	}
+}
