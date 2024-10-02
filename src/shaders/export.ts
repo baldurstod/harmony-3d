365dd2226d249e6b179463b5a20447adc11de93d
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-02 17:39:46 +0200

    Changed import order

--- src/shaders/export.ts
+++ src/shaders/export.ts
@@ -1,7 +1,6 @@
-export * from './includemanager';
-export { Includes } from './includes';
-export { Shaders } from './shaders';
-export { ShaderEditor } from './shadereditor';
-
 export * from './includes/export';
 export * from './shaders/export';
+export * from './includemanager';
+export { Includes } from './includes';
+export { ShaderEditor } from './shadereditor';
+export { Shaders } from './shaders';
