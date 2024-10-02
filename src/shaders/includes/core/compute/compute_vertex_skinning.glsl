4cd4a1243ec13d45c808f310cb1b4acb5252a347
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-02 16:41:20 +0200

    Added compute_vertex_skinning.glsl

---
+++ src/shaders/includes/core/compute/compute_vertex_skinning.glsl
@@ -0,0 +1,33 @@
+export default `
+#ifdef SKELETAL_MESH
+	#ifdef HARDWARE_SKINNING
+		mat4 skinMat = accumulateSkinMat();
+		vec4 vertexPositionWorldSpace = skinMat * vertexPositionModelSpace;
+		vec3 vertexNormalWorldSpace = vec3(skinMat * vertexNormalModelSpace).xyz;
+		vec3 vertexTangentWorldSpace = vec3(skinMat * vertexTangentModelSpace).xyz;
+	#else
+		#define vertexPositionWorldSpace vertexPositionModelSpace
+		#define vertexNormalWorldSpace vertexNormalModelSpace.xyz
+		#define vertexTangentWorldSpace vertexTangentModelSpace.xyz
+	#endif
+#else
+	vec4 vertexPositionWorldSpace = uModelMatrix * vertexPositionModelSpace;
+	vec3 vertexNormalWorldSpace = vec3(uModelMatrix * vertexNormalModelSpace).xyz;
+	vec3 vertexTangentWorldSpace = vec3(uModelMatrix * vertexTangentModelSpace).xyz;
+#endif
+vVertexPositionWorldSpace = vertexPositionWorldSpace;
+vVertexNormalWorldSpace = vertexNormalWorldSpace;
+#ifdef TESTING
+	vVertexTangentWorldSpace = vertexTangentWorldSpace;
+#endif
+
+#ifdef USE_VERTEX_TANGENT
+	vec3 vertexBitangentWorldSpace = cross( vertexNormalWorldSpace, vertexTangentWorldSpace) * aVertexTangent.w;
+#else
+	//TODO: compute it properly
+	vec3 vertexBitangentWorldSpace = cross( vertexNormalWorldSpace, vertexTangentWorldSpace) * -1.0;
+#endif
+#ifdef TESTING
+	vVertexBitangentWorldSpace = vertexBitangentWorldSpace;
+#endif
+`;
--- src/shaders/includes/core/compute/export.ts
+++ src/shaders/includes/core/compute/export.ts
@@ -1,5 +1,8 @@
 import { Includes } from '../../../../shaders/includes';
 
+import compute_vertex_skinning from './compute_vertex_skinning.glsl';
+Includes['compute_vertex_skinning'] = compute_vertex_skinning;
+
 import compute_vertex_uv from './compute_vertex_uv.glsl';
 Includes['compute_vertex_uv'] = compute_vertex_uv;
 
