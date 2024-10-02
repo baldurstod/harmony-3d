ce9a4f1647cc48ff58ce279fcc955bcafba6b59c
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-03 00:41:48 +0200

    Simplified Target

--- src/objects/target.ts
+++ src/objects/target.ts
@@ -3,19 +3,11 @@ import { Entity } from '../entities/entity';
 
 // It is just a basic entity
 export class Target extends Entity {
-	constructor(params?: any) {
-		super(params);
-	}
-
 	static async constructFromJSON(json: any) {
 		return new Target({ name: json.name });
 	}
 
-	get entityName() {
-		return 'Target';
-	}
-
-	static get entityName() {
+	static getEntityName() {
 		return 'Target';
 	}
 }
