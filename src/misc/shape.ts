0f8a1116c0a35d04daa3bee5b617f2b26f7b9ea2
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-03 00:13:24 +0200

    Added Shape

---
+++ src/misc/shape.ts
@@ -0,0 +1,93 @@
+import { Path } from '../math/curves/path';
+import { generateRandomUUID } from '../math/functions'
+
+export class Shape extends Path {
+	uuid = generateRandomUUID();
+	type = 'Shape';
+	holes = [];
+
+	getPointsHoles(divisions) {
+
+		const holesPts = [];
+
+		for (let i = 0, l = this.holes.length; i < l; i++) {
+
+			holesPts[i] = this.holes[i].getPoints(divisions);
+
+		}
+
+		return holesPts;
+
+	}
+
+	// get points of shape and holes (keypoints based on segments parameter)
+
+	extractPoints(divisions) {
+
+		return {
+
+			shape: this.getPoints(divisions),
+			holes: this.getPointsHoles(divisions)
+
+		};
+
+	}
+/*
+	copy(source) {
+
+		super.copy(source);
+
+		this.holes = [];
+
+		for (let i = 0, l = source.holes.length; i < l; i++) {
+
+			const hole = source.holes[i];
+
+			this.holes.push(hole.clone());
+
+		}
+
+		return this;
+
+	}
+		*/
+/*
+	toJSON() {
+
+		const data = super.toJSON();
+
+		data.uuid = this.uuid;
+		data.holes = [];
+
+		for (let i = 0, l = this.holes.length; i < l; i++) {
+
+			const hole = this.holes[i];
+			data.holes.push(hole.toJSON());
+
+		}
+
+		return data;
+
+	}
+		*/
+/*
+	fromJSON(json) {
+
+		super.fromJSON(json);
+
+		this.uuid = json.uuid;
+		this.holes = [];
+
+		for (let i = 0, l = json.holes.length; i < l; i++) {
+
+			const hole = json.holes[i];
+			this.holes.push(new Path().fromJSON(hole));
+
+		}
+
+		return this;
+
+	}
+*/
+
+}
