314730d5cb21ae3f6a1fd119c3e8c616c3efcf61
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-02 23:58:59 +0200

    Added Curve

---
+++ src/math/curves/curve.ts
@@ -0,0 +1,45 @@
+import { vec3 } from 'gl-matrix';
+
+export const DEFAULT_POINT = vec3.create();
+
+export class Curve {
+	controlPoints = [];
+	arcLength = 0;
+
+	getPosition(t, out = vec3.create()) {
+		return out;
+	}
+
+	getArcLength(divisions = 100) {
+		let pos = vec3.create();
+
+		let last = vec3.create();
+		let current = vec3.create();
+		let temp;
+
+		this.getPosition(0, last);
+		let length = 0;
+
+		for (let i = 1; i <= divisions; i++) {
+			this.getPosition(i / divisions, current);
+			length += vec3.distance(last, current);
+			temp = last;
+			last = current;
+			current = temp;
+		}
+		return length;
+	}
+
+	getPoints(divisions = 5) {
+		let points = [];
+
+		for (let i = 0; i <= divisions; i++) {
+			points.push(this.getPosition(i / divisions));
+		}
+		return points;
+	}
+
+	getAppropriateDivision(division) {
+		return division;
+	}
+}
