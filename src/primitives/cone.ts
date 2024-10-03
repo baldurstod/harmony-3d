623ff623ab59f34a75356c0df90f58daf5165ce1
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-03 21:58:45 +0200

    Added Cone

---
+++ src/primitives/cone.ts
@@ -0,0 +1,37 @@
+import { ConeBufferGeometry } from './geometries/conebuffergeometry';
+import { MeshBasicMaterial } from '../materials/meshbasicmaterial';
+import { Mesh } from '../objects/mesh';
+import { registerEntity } from '../entities/entities';
+
+export class Cone extends Mesh {
+	#radius: number;
+	#height: number;
+	#segments: number;
+	#hasCap: boolean;
+	constructor(params: any = {}) {
+		super(new ConeBufferGeometry(), params.material ?? new MeshBasicMaterial());
+		super.setParameters(params);
+		this.#radius = params.radius ?? 1;
+		this.#height = params.height ?? 1;
+		this.#segments = params.segments ?? 24;
+		this.#hasCap = params.hasCap ?? true;
+		this.#updateGeometry();
+	}
+
+	#updateGeometry() {
+		(this.geometry as ConeBufferGeometry).updateGeometry(this.#radius, this.#height, this.#segments, this.#hasCap);
+	}
+
+	buildContextMenu() {
+		return Object.assign(super.buildContextMenu(), {
+			Cone_1: null,
+			radius: { i18n: '#radius', f: () => { let radius = prompt('Radius', String(this.#radius)); if (radius) { this.#radius = Number(radius); this.#updateGeometry(); } } },
+			height: { i18n: '#height', f: () => { let height = prompt('Height', String(this.#height)); if (height) { this.#height = Number(height); this.#updateGeometry(); } } },
+		});
+	}
+
+	static getEntityName() {
+		return 'Cone';
+	}
+}
+registerEntity(Cone);
---
+++ src/primitives/geometries/conebuffergeometry.ts
@@ -0,0 +1,90 @@
+import { vec3 } from 'gl-matrix';
+
+import { Float32BufferAttribute, Uint16BufferAttribute } from '../../geometry/bufferattribute'
+import { BufferGeometry } from '../../geometry/buffergeometry';
+import { TWO_PI } from '../../math/constants';
+
+
+export class ConeBufferGeometry extends BufferGeometry {
+	#indices: number[];
+	#vertices: number[];
+	#normals: number[];
+	#uvs: number[];
+
+	updateGeometry(radius = 1, height = 1, segments = 24, hasCap = true) {
+		segments = Math.max(Math.floor(segments), 3);
+		// buffers
+
+		this.#indices = [];
+		this.#vertices = [];
+		this.#normals = [];
+		this.#uvs = [];
+
+		this.#generateCone(radius, height, segments);
+		if (hasCap) {
+			this.#generateCap(radius, 0, segments);
+			//this.generateCap(radius, +height / 2, segments);
+		}
+
+		// build geometry
+		this.setIndex(new Uint16BufferAttribute(this.#indices, 1));
+		this.setAttribute('aVertexPosition', new Float32BufferAttribute(this.#vertices, 3));
+		this.setAttribute('aVertexNormal', new Float32BufferAttribute(this.#normals, 3));
+		this.setAttribute('aTextureCoord', new Float32BufferAttribute(this.#uvs, 2));
+		this.count = this.#indices.length;
+	}
+
+	#generateCone(radius, height, segments) {
+		var normal = vec3.create();
+		var vertex = vec3.create();
+
+		let thetaPerSegment = TWO_PI / segments;
+		let vPerSegment = 1 / segments;
+		//let halfHeight = height / 2.0;
+
+		for (let segmentId = 0; segmentId <= segments; ++segmentId) {
+			let theta = thetaPerSegment * segmentId;
+			let sinTheta = Math.sin(theta);
+			let cosTheta = Math.cos(theta);
+			vertex[0] = radius * cosTheta;
+			vertex[1] = radius * sinTheta;
+			let u = segmentId / segments;
+
+			normal[0] = cosTheta;
+			normal[1] = sinTheta;
+			//TODO: compute proper normal
+
+			// Bottom vertex
+			vertex[2] = 0;
+			this.#vertices.push(...vertex);
+			this.#normals.push(...normal);
+			this.#uvs.push(u, 0);
+
+			// Top vertex
+			this.#vertices.push(0, 0, height);
+			this.#normals.push(...normal);
+			this.#uvs.push(u, 1);
+
+			let indexStart = segmentId * 2;
+			this.#indices.push(indexStart, indexStart + 2, indexStart + 1);
+			this.#indices.push(indexStart + 1, indexStart + 2, indexStart + 3);
+		}
+	}
+
+	#generateCap(radius, z, segments) {
+		let middlePointIndex = this.#vertices.length / 3;
+
+		// Push middle vertex
+		this.#vertices.push(0, 0, z);
+		this.#normals.push(0, 0, Math.sign(z));
+		this.#uvs.push(0.5, 0.5);
+
+		// Note: we use vertices generated in generateCone for the caps.
+		// This uses less memory but limits uv / normals usage
+		let indexStart = z <= 0 ? 0 : 1;
+		for (let segmentId = 0; segmentId <= segments; ++segmentId) {
+			this.#indices.push(indexStart, middlePointIndex, indexStart + 2);
+			indexStart += 2;
+		}
+	}
+}
