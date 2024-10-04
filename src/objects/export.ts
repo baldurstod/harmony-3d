451e666b6d36cf5dee0845d917fad72d86719544
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-05 00:52:44 +0200

    Added Skeleton

--- src/objects/export.ts
+++ src/objects/export.ts
@@ -1,6 +1,8 @@
 export * from './helpers/export';
 export { Decal } from './decal';
 export { Group } from './group';
+export { SkeletalMesh } from './skeletalmesh';
+export { Skeleton } from './skeleton';
 export { Mesh } from './mesh';
 export { Target } from './target';
 export { Text3D } from './text3d';
---
+++ src/objects/skeleton.ts
@@ -0,0 +1,209 @@
+import { mat4, quat, vec3 } from 'gl-matrix';
+
+import { Bone } from './bone';
+import { Graphics } from '../graphics/graphics';//TODOv3: removeme
+import { Entity } from '../entities/entity';
+import { TextureManager } from '../textures/texturemanager'
+import { GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_TEXTURE_MIN_FILTER, GL_NEAREST, GL_FLOAT, GL_RGBA, GL_RGBA32F, } from '../webgl/constants';
+import { BoundingBox } from '../math/boundingbox';
+import { MAX_HARDWARE_BONES } from '../constants';
+import { Texture } from '../textures/texture';
+import { registerEntity } from '../entities/entities';
+
+const identityMatrix = mat4.create();
+export class Skeleton extends Entity {
+	isSkeleton = true;
+	#bonesByName = new Map();
+	#rootBone = new Bone({ name: 'root', boneId: 0, skeleton: this });
+	_bones = [];//TODOv3: rename
+	_dirty = true;
+	#imgData: Float32Array;
+	#texture: Texture;
+	constructor(parameters) {
+		super(parameters);
+		//this.bones = Object.create(null);//TODOv3: rename
+
+		this.createBoneMatrixArray();
+		this.createBoneMatrixTexture();
+		this.dirty();
+	}
+
+	dirty() {
+		this._dirty = true;
+		for (let bone of this._bones) {
+			bone.dirty = true;
+		}
+		/*if (this._bones[0]) {
+			this._bones[0].dirty = true;
+		}*/
+	}
+
+	getTexture() {
+		return this.#texture;
+	}
+
+	createBoneMatrixArray() {
+		this.#imgData = new Float32Array(MAX_HARDWARE_BONES * 4 * 4);
+		mat4.identity(this.#imgData);
+		let index = 0;
+		for (let i = 1; i < MAX_HARDWARE_BONES; ++i) {
+			this.#imgData.copyWithin(i * 16, 0, 16);
+		}
+	}
+
+	createBoneMatrixTexture() {
+		this.#texture = TextureManager.createTexture();
+		const gl = Graphics.glContext;//TODO
+		gl.bindTexture(GL_TEXTURE_2D, this.#texture.texture);//TODOv3: pass param to texture and remove this
+		gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		gl.bindTexture(GL_TEXTURE_2D, null);
+	}
+
+	updateBoneMatrixTexture() {//removeme
+		const gl = Graphics.glContext;//TODO
+		gl.bindTexture(GL_TEXTURE_2D, this.#texture.texture);
+		if (Graphics.isWebGL2) {
+			gl.texImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, 4, MAX_HARDWARE_BONES, 0, GL_RGBA, GL_FLOAT, this.#imgData);
+		} else {
+			gl.texImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 4, MAX_HARDWARE_BONES, 0, GL_RGBA, GL_FLOAT, this.#imgData);
+		}
+		gl.bindTexture(GL_TEXTURE_2D, null);
+	}
+
+	setBonesMatrix() {
+		let index = 0;
+		let bones = this._bones;
+		let imgData = this.#imgData;
+
+		let pose;
+		if (bones.length == 0) {
+			pose = this.#rootBone.boneMat;
+			for (let k = 0; k < 16; ++k) {
+				imgData[index++] = pose[k];
+			}
+		}
+
+		for (let bone of bones) {
+			pose = bone.boneMat;
+			for (let k = 0; k < 16; ++k) {
+				imgData[index++] = pose[k];
+			}
+		}
+
+		this.updateBoneMatrixTexture();
+	}
+
+	set position(position) {
+		super.position = position;
+	}
+
+	get position() {
+		if (this._parent) {
+			return vec3.clone(this._parent._position);
+		} else {
+			return vec3.clone(this._position);
+		}
+	}
+
+	set quaternion(quaternion) {
+		super.quaternion = quaternion;
+	}
+
+	get quaternion() {
+		if (this._parent) {
+			return quat.clone(this._parent._quaternion);
+		} else {
+			return quat.clone(this._quaternion);
+		}
+	}
+
+	addBone(boneId, boneName) {
+		let boneNameLowerCase = boneName.toLowerCase();
+		if (!this.#bonesByName.has(boneNameLowerCase)) {
+			let bone = new Bone({ name: boneName, boneId: boneId });
+			bone.skeleton = this;
+			//this.addChild(bone);
+			this._bones[boneId] = bone;
+			this.#bonesByName.set(boneNameLowerCase, bone);
+			return bone;
+		} else {
+			let bone = this.#bonesByName.get(boneNameLowerCase);
+			this._bones[boneId] = bone;
+			return bone;
+		}
+	}
+
+	async setParentSkeleton(skeleton) {
+		await this.loadedPromise;
+		if (skeleton) {
+			await skeleton.loadedPromise;
+		}
+		let bones = this.#bonesByName;
+		for (let [boneName, bone] of bones) {
+			bone.parentSkeletonBone = skeleton ? skeleton.getBoneByName(boneName) : null;
+		}
+	}
+
+	getBoneByName(boneName) {
+		return this.#bonesByName.get(boneName.toLowerCase());
+	}
+
+	getBoneById(boneId) {
+		return this._bones[boneId];
+	}
+
+	toString() {
+		return 'Skeleton ' + super.toString();
+	}
+
+	getBoundingBox(boundingBox = new BoundingBox()) {
+		boundingBox.reset();
+		return boundingBox;
+	}
+
+	get bones() {
+		return this._bones;
+	}
+
+	toJSON() {
+		let json = super.toJSON();
+		let jBones = [];
+		let bones = this._bones;
+		for (let i = 0; i < bones.length; ++i) {
+			jBones.push(bones[i].id);
+		}
+		json.bones = jBones;
+		return json;
+	}
+
+	static async constructFromJSON(json, entities, loadedPromise) {
+		let entity = new Skeleton({ name: json.name });
+		let loadedPromiseResolve;
+		entity.loadedPromise = new Promise((resolve) => loadedPromiseResolve = resolve);
+		loadedPromise.then(() => {
+			let jBones = json.bones;
+			if (jBones) {
+				for (let i = 0; i < jBones.length; ++i) {
+					let boneEntity = entities.get(jBones[i]);
+					if (boneEntity) {
+						entity._bones[i] = boneEntity;
+						entity.#bonesByName.set(boneEntity.name.toLowerCase(), boneEntity);
+					}
+				}
+			}
+			loadedPromiseResolve(true);
+		});
+		return entity;
+	}
+
+	dispose() {
+		super.dispose();
+		this.#texture.dispose();
+	}
+
+	static getEntityName() {
+		return 'Skeleton';
+	}
+}
+registerEntity(Skeleton);
