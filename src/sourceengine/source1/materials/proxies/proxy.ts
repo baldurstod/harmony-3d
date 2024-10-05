1cb84b067745979da7d1a8531df549ca4dfe5dfd
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-06 00:25:39 +0200

    Added ProxyManager, Proxy

---
+++ src/sourceengine/source1/materials/proxies/proxy.ts
@@ -0,0 +1,100 @@
+/*export setResult(variables, value) {
+	let resultVarName = this.getData('resultvar');
+	if (resultVarName) {
+		resultVarName = resultVarName.toLowerCase();
+		if (resultVarName.indexOf('[') != -1) {
+			let result = (/([^\[]*)\[(\d*)\]/g).exec(resultVarName);
+			if (result && result.length == 3) {
+				const v = variables.get(result[1].toLowerCase());
+				if (v) {
+					v[result[2]] = value;
+				}
+			}
+		} else {
+			variables.set(resultVarName, value);
+		}
+	}
+}
+*/
+/**
+ * Source engine material interface
+ */
+export class Proxy {
+	datas:any = null;
+
+	/**
+	 * TODO
+	 */
+	setParams(datas, variables) {
+		this.datas = datas;
+		this.init(variables);
+	}
+
+	/**
+	 * TODO
+	 */
+	getData(name) {
+		const result = this.datas[name];
+		if (typeof result == 'string') {
+			return result.toLowerCase();
+		}
+		return result;
+	}
+
+	/**
+	 * Dummy function
+	 */
+	init(variables) {
+	}
+
+	/**
+	 * Dummy function
+	 */
+	execute(variables, proxyParams, time) {
+	}
+
+	setResult(variables, value) {
+		let resultVarName = this.getData('resultvar');
+		if (resultVarName) {
+			resultVarName = resultVarName.toLowerCase();
+			if (resultVarName.indexOf('[') != -1) {
+				let result = (/([^\[]*)\[(\d*)\]/g).exec(resultVarName);
+				if (result && result.length == 3) {
+					const v = variables.get(result[1].toLowerCase());
+					if (v) {
+						v[result[2]] = value;
+					}
+				}
+			} else {
+				let v = variables.get(resultVarName);
+				if ((v === undefined) && (value instanceof Array || value instanceof Float32Array)) {
+					v = new Float32Array(value.length)
+					variables.set(resultVarName, v);
+				}
+
+				if (v instanceof Array || v instanceof Float32Array) {
+					v[0] = value?.[0] ?? value;
+					v[1] = value?.[1] ?? value;
+					v[2] = value?.[2] ?? value;
+				} else {
+					variables.set(resultVarName, value);
+				}
+
+			}
+		}
+	}
+
+	getVariable(variables, name) {
+		const result = this.datas[name];
+		if (typeof result == 'string') {
+			if (result.startsWith('$')) {
+				const varName = result.toLowerCase();
+				return variables.get(varName);
+
+			} else {
+				return result.toLowerCase();
+			}
+		}
+		return result;
+	}
+}
---
+++ src/sourceengine/source1/materials/proxies/proxymanager.ts
@@ -0,0 +1,25 @@
+/**
+ * Proxy manager
+ */
+export class ProxyManager {
+	static #proxyList = {};
+
+	static getProxy(proxyName) {
+		if (!proxyName) {
+			return;
+		}
+		proxyName = proxyName.toLowerCase();
+		const proxy = this.#proxyList[proxyName];
+		if (!proxy) {
+			return null;
+		}
+		return new proxy();
+	}
+
+	static registerProxy(proxyName, proxyClass) {
+		if (!proxyClass) { return; }
+		const name = proxyName.toLowerCase();
+
+		this.#proxyList[name] = proxyClass;
+	}
+}
