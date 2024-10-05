9788f03a83038ff5dd285e5b1941dc76c349f865
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-05 13:44:51 +0200

    Added getRandomInt()

--- src/utils/export.ts
+++ src/utils/export.ts
@@ -1,4 +1,5 @@
 export * from './customfetch';
+export { getRandomInt } from './random';
 export * from './utils';
 export { RenderTargetViewer } from './rendertargetviewer';
 export { WebGLStats } from './webglstats';
