26cf48ee827e05a83d7ac9067ad5e16b0b45efe7
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-05 13:03:50 +0200

    Added Animations

---
+++ src/animations/animations.ts
@@ -0,0 +1,42 @@
+export class Animations {
+	#animations = new Map();
+	constructor() {
+	}
+
+	add(animation) {
+		this.#animations.set(animation.name, animation);
+		this.#computeWeights();
+	}
+
+	delete(animation) {
+		this.#animations.delete(animation.name);
+		this.#computeWeights();
+	}
+
+	get animations() {
+		return this.#animations;
+	}
+
+	get(animationName) {
+		return this.#animations.get(animationName);
+	}
+
+	setWeight(animationName, weight) {
+		let animation = this.#animations.get(animationName);
+		if (!animation) {
+			return false;
+		}
+
+		animation.weight = weight;
+		this.#computeWeights();
+		return true;
+	}
+
+	#computeWeights() {
+
+	}
+
+	play(delta) {
+
+	}
+}
