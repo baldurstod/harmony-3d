f92082edb7f7315f3ed9532c75cb1bea25893cfb
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-06 17:41:38 +0200

    Added declare_fragment_detail_map.glsl

---
+++ src/shaders/includes/declare/declare_fragment_detail_map.glsl
@@ -0,0 +1,5 @@
+export default `
+#ifdef USE_DETAIL_MAP
+	uniform sampler2D detailMap;
+#endif
+`;
--- src/shaders/includes/declare/export.ts
+++ src/shaders/includes/declare/export.ts
@@ -3,6 +3,9 @@ import { Includes } from '../../../shaders/includes';
 import declare_fragment_color_map from './declare_fragment_color_map.glsl';
 Includes['declare_fragment_color_map'] = declare_fragment_color_map;
 
+import declare_fragment_detail_map from './declare_fragment_detail_map.glsl';
+Includes['declare_fragment_detail_map'] = declare_fragment_detail_map;
+
 import declare_fragment_diffuse from './declare_fragment_diffuse.glsl';
 Includes['declare_fragment_diffuse'] = declare_fragment_diffuse;
 
