4f24ba8e998853712e2650b8a0e3fc040c3c8881
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-06 18:38:56 +0200

    Added MeshPhongMaterial

---
+++ src/materials/meshphongmaterial.ts
@@ -0,0 +1,31 @@
+import { Material } from './material';
+
+export class MeshPhongMaterial extends Material{
+	map = null;
+	lightMap = null;
+	lightMapIntensity = 1.0;
+	aoMap = null;
+	aoMapIntensity = 1.0;
+	specularMap = null;
+	alphaMap = null;
+	envMap = null;
+	combine = 0/*MultiplyOperation*/;
+	reflectivity = 1;
+	refractionRatio = 0.98;
+	wireframe = false;
+	wireframeLinewidth = 1;
+	wireframeLinecap = 'round';
+	wireframeLinejoin = 'round';
+	skinning = false;
+	morphTargets = false;
+
+	constructor(params: any = {}) {
+		super(params);
+		this.setValues(params);
+	}
+
+	getShaderSource() {
+		return 'meshphong';
+	}
+}
+Material.materialList['MeshPhong'] = MeshPhongMaterial;
