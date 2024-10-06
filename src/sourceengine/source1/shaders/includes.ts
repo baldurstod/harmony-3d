d17cfa81c358fdfbf25b9bfd860157dbf11a4952
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-06 14:09:39 +0200

    Added source1_declare_phong.glsl

--- src/sourceengine/source1/shaders/includes.ts
+++ src/sourceengine/source1/shaders/includes.ts
@@ -1,5 +1,8 @@
 import { Includes } from '../../../shaders/includes';
 
+import source1_declare_phong from './includes/source1_declare_phong.glsl';
+Includes['source1_declare_phong'] = source1_declare_phong;
+
 import source1_fragment_common from './includes/source1_fragment_common.glsl';
 Includes['source1_fragment_common'] = source1_fragment_common;
 
---
+++ src/sourceengine/source1/shaders/includes/source1_declare_phong.glsl
@@ -0,0 +1,6 @@
+export default `
+//TODO: set a vec2 for these two ?
+uniform float uPhongExponent;
+uniform float uPhongExponentFactor;
+uniform float uPhongBoost;
+`;
