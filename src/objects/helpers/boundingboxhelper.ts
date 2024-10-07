184e34898af8e446ffba72133ef697f8286e03d1
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-07 13:09:01 +0200

    Added BoundingBoxHelper

---
+++ src/objects/helpers/boundingboxhelper.ts
@@ -0,0 +1,38 @@
+import { quat, vec3 } from 'gl-matrix';
+
+import { BoundingBox } from '../../math/boundingbox';
+import { Box } from '../../primitives/box';
+
+const tempVec3 = vec3.create();
+
+export class BoundingBoxHelper extends Box {
+	boundingBox = new BoundingBox();
+	constructor(params: any = {}) {
+		super(params);
+		this.wireframe = true;
+	}
+
+	update() {
+		if (this._parent) {
+			this._parent.getBoundingBox(this.boundingBox);
+
+			this.boundingBox.getCenter(this._position);
+			this.boundingBox.getSize(tempVec3);
+
+			this.setSize(tempVec3[0], tempVec3[1], tempVec3[2]);
+		}
+	}
+
+	getWorldPosition(vec = vec3.create()) {
+		return vec3.copy(vec, this._position);
+	}
+
+	getWorldQuaternion(q = quat.create()) {
+		return quat.identity(q);
+	}
+
+	getBoundingBox(boundingBox = new BoundingBox()) {
+		boundingBox.reset();
+		return boundingBox;
+	}
+}
--- src/objects/helpers/export.ts
+++ src/objects/helpers/export.ts
@@ -1,4 +1,4 @@
-
+export { BoundingBoxHelper } from './boundingboxhelper';
 export { CameraFrustum } from './camerafrustum';
 export { Grid } from './grid';
 export { Manipulator } from './manipulator';
