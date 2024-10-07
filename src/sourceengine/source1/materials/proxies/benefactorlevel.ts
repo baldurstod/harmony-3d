198a967a3ee823c3cf6f1603e0728974520abd24
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-07 22:22:33 +0200

    Added BenefactorLevel proxy

---
+++ src/sourceengine/source1/materials/proxies/benefactorlevel.ts
@@ -0,0 +1,23 @@
+import { ProxyManager } from './proxymanager';
+
+const minValue = 5.0;
+const maxValue = 1.0;
+
+export class BenefactorLevel {
+	#datas;
+	#resultVar;
+	setParams(datas) {
+		this.#datas = datas;
+		this.init();
+	}
+
+	init() {
+		this.#resultVar = this.#datas['resultvar'];
+	}
+
+	execute(variables) {
+		let value = 1.0;
+		variables.set(this.#resultVar, minValue + ( maxValue - minValue ) * value);
+	}
+}
+ProxyManager.registerProxy('BenefactorLevel', BenefactorLevel);
--- src/sourceengine/source1/materials/proxies/export.ts
+++ src/sourceengine/source1/materials/proxies/export.ts
@@ -2,4 +2,5 @@ export { ProxyManager } from './proxymanager';
 export { Add } from './add';
 export { AnimatedTextureProxy } from './animatedtexture';
 export { AnimatedWeaponSheen } from './animatedweaponsheen';
+export { BenefactorLevel } from './benefactorlevel';
 export { WeaponSkin } from './weaponskin';
