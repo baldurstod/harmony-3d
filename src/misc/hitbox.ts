e77912b72f2492f103e5922c7fdf6fa2f55d2a7b
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-08 09:34:50 +0200

    Hitbox: removed private properties

--- src/misc/hitbox.ts
+++ src/misc/hitbox.ts
@@ -1,14 +1,15 @@
 import { vec3 } from 'gl-matrix';
+import { Entity } from '../entities/entity';
 
 export class Hitbox {
-	#name: string;
-	#boundingBoxMin = vec3.create();
-	#boundingBoxMax = vec3.create();
-	#parent = vec3.create();
-	constructor(name: string, boundingBoxMin: vec3, boundingBoxMax: vec3, parent: vec3/*TODO: change to actual type*/) {
-		this.#name = name;
-		vec3.copy(this.#boundingBoxMin, boundingBoxMin);
-		vec3.copy(this.#boundingBoxMax, boundingBoxMax);
-		this.#parent = parent;
+	name: string;
+	boundingBoxMin = vec3.create();
+	boundingBoxMax = vec3.create();
+	parent: Entity;
+	constructor(name: string, boundingBoxMin: vec3, boundingBoxMax: vec3, parent: Entity) {
+		this.name = name;
+		vec3.copy(this.boundingBoxMin, boundingBoxMin);
+		vec3.copy(this.boundingBoxMax, boundingBoxMax);
+		this.parent = parent;
 	}
 }
