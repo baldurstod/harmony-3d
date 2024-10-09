0e7ce03c9242906ba5090e61d281b1f5eaebfafc
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-09 21:41:48 +0200

    Added ColorRandom operator

---
+++ src/sourceengine/source1/particles/operators/initializers/colorrandom.ts
@@ -0,0 +1,19 @@
+import { SourceEngineParticleOperators } from '../../sourceengineparticleoperators';
+import { SourceEngineParticleOperator } from '../operator';
+import { PARAM_TYPE_COLOR } from '../../constants';
+import { BLACK, Color, WHITE } from '../../color';
+export class ColorRandom extends SourceEngineParticleOperator {
+	static functionName = 'Color Random';
+
+	constructor() {
+		super();
+		this.addParam('color1', PARAM_TYPE_COLOR, BLACK);
+		this.addParam('color2', PARAM_TYPE_COLOR, WHITE);
+	}
+
+	doInit(particle, elapsedTime) {
+		particle.color.randomize(this.getParameter('color1'), this.getParameter('color2'));
+		particle.initialColor.setColor(particle.color);
+	}
+}
+SourceEngineParticleOperators.registerOperator(ColorRandom);
