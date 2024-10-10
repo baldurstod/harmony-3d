b44bce70c38ab7954425dbf33a80ebfc44d3d7cf
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-10 13:43:39 +0200

    Added RenderPass

---
+++ src/composer/passes/renderpass.ts
@@ -0,0 +1,20 @@
+import { Camera } from '../cameras/camera';
+import { Scene } from '../scenes/scene';
+import { Pass } from './pass';
+
+export class RenderPass extends Pass {
+	scene: Scene;
+	constructor(scene: Scene, camera: Camera) {
+		super();
+		this.swapBuffers = false;
+		this.scene = scene;
+		this.camera = camera;
+	}
+
+	render(renderer, readBuffer, writeBuffer, renderToScreen, delta) {
+
+		renderer.pushRenderTarget(renderToScreen ? null : writeBuffer);
+		renderer.render(this.scene, this.camera, delta);
+		renderer.popRenderTarget();
+	}
+}
