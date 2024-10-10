1f099ebbb203f3b75ad8b9a68a55ce043808447b
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-10 13:42:08 +0200

    Added CrosshatchPass

---
+++ src/composer/passes/crosshatchpass.ts
@@ -0,0 +1,22 @@
+import { Pass } from './pass';
+import { ShaderMaterial } from '../materials/shadermaterial';
+import { FullScreenQuad } from '../primitives/fullscreenquad';
+
+export class CrosshatchPass extends Pass {
+	constructor(camera) {//TODO: camera is not really needed
+		super();
+		let material = new ShaderMaterial({ shaderSource: 'crosshatch' });
+		material.addUser(this);
+		material.depthTest = false;
+		this.quad = new FullScreenQuad({ material: material });
+		this.camera = camera;
+	}
+
+	render(renderer, readBuffer, writeBuffer, renderToScreen) {
+		this.quad.material.uniforms['colorMap'] = readBuffer.getTexture();
+
+		renderer.pushRenderTarget(renderToScreen ? null : writeBuffer);
+		renderer.render(this.quad, this.camera);
+		renderer.popRenderTarget();
+	}
+}
