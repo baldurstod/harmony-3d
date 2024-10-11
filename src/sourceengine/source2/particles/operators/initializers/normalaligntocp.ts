4052a953af1e82e498dfbb28c2fcedcc9278fec7
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-11 22:53:30 +0200

    Added NormalAlignToCP

---
+++ src/sourceengine/source2/particles/operators/initializers/normalaligntocp.ts
@@ -0,0 +1,27 @@
+import { vec3 } from 'gl-matrix';
+import { RegisterSource2ParticleOperator } from '../source2particleoperators';
+import { Operator } from '../operator';
+
+//Notice this is not the default particle normal.
+//This operator change the default normal from +Z to +X
+const DEFAULT_NORMAL = vec3.fromValues(1, 0, 0);
+
+export class NormalAlignToCP extends Operator {
+
+	_paramChanged(paramName, value) {
+		switch (paramName) {
+			default:
+				super._paramChanged(paramName, value);
+		}
+	}
+
+	doInit(particle, elapsedTime) {
+		//This operator overrides the normal
+		let cp = this.system.getControlPoint(this.controlPointNumber);
+		if (cp) {
+			//cp.getWorldTransformation(particle.cpPreviousTransform);
+			vec3.transformQuat(particle.normal, DEFAULT_NORMAL, cp.currentWorldQuaternion);
+		}
+	}
+}
+RegisterSource2ParticleOperator('C_INIT_NormalAlignToCP', NormalAlignToCP);
