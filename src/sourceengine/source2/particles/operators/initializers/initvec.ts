dd5d52117db606e2e9120248eeda917a5f96390c
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-11 22:53:18 +0200

    Added InitVec

---
+++ src/sourceengine/source2/particles/operators/initializers/initvec.ts
@@ -0,0 +1,38 @@
+import { vec3, vec4 } from 'gl-matrix';
+import { RegisterSource2ParticleOperator } from '../source2particleoperators';
+import { Operator } from '../operator';
+import { PARTICLE_FIELD_COLOR } from '../../../../common/particles/particlefields';
+
+const DEFAULT_INPUT_VALUE = vec3.create();
+let v = vec4.create();
+
+export class InitVec extends Operator {
+	setMethod = null;
+	scaleInitialRange = false;
+	fieldOutput = PARTICLE_FIELD_COLOR;
+
+	_paramChanged(paramName, value) {
+		switch (paramName) {
+			case 'm_InputValue':
+				break;
+			case 'm_nOutputField':
+				this.fieldOutput = Number(value);
+				break;
+			case 'm_nSetMethod':
+				this.setMethod = value;
+				break;
+			case 'm_bScaleInitialRange':
+				this.scaleInitialRange = value;
+				break;
+			default:
+				super._paramChanged(paramName, value);
+		}
+	}
+
+	doInit(particle, elapsedTime) {
+		let inputValue = this.getParamVectorValue('m_InputValue', particle, v) ?? DEFAULT_INPUT_VALUE;
+
+		particle.setField(this.fieldOutput, inputValue, this.scaleInitialRange || this.setMethod == 'PARTICLE_SET_SCALE_INITIAL_VALUE');
+	}
+}
+RegisterSource2ParticleOperator('C_INIT_InitVec', InitVec);
