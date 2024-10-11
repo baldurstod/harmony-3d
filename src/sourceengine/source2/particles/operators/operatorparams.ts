aaf632dc3ea8ae5ec226026b5bc80f180e433d9b
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-11 22:06:45 +0200

    Added source2 operator params

---
+++ src/sourceengine/source2/particles/operators/operatorparams.ts
@@ -0,0 +1,27 @@
+export const OPERATOR_PARAM_DISABLE_OPERATOR = 'm_bDisableOperator';
+
+export const OPERATOR_PARAM_EVEN_DISTRUBUTION = 'm_bEvenDistribution';
+export const OPERATOR_PARAM_MOD_2X = 'm_bMod2X';
+
+export const OPERATOR_PARAM_COLOR_MAX = 'm_ColorMax';
+export const OPERATOR_PARAM_COLOR_MIN = 'm_ColorMin';
+
+export const OPERATOR_PARAM_DEGREES_MAX = 'm_flDegreesMax';
+export const OPERATOR_PARAM_DEGREES_MIN = 'm_flDegreesMin';
+export const OPERATOR_PARAM_INITIAL_RADIUS = 'm_flInitialRadius';
+export const OPERATOR_PARAM_INITIAL_SPEED_MAX = 'm_flInitialSpeedMax';
+export const OPERATOR_PARAM_INITIAL_SPEED_MIN = 'm_flInitialSpeedMin';
+export const OPERATOR_PARAM_RADIUS_MAX = 'm_flRadiusMax';
+export const OPERATOR_PARAM_RADIUS_MIN = 'm_flRadiusMin';
+
+export const OPERATOR_PARAM_TEXTURE = 'm_hTexture';
+
+
+export const OPERATOR_PARAM_ALPHA_MAX = 'm_nAlphaMax';
+export const OPERATOR_PARAM_ALPHA_MIN = 'm_nAlphaMin';
+
+export const OPERATOR_PARAM_ORIENTATION_TYPE = 'm_nOrientationType';
+export const OPERATOR_PARAM_SEQUENCE_COMBINE_MODE = 'm_nSequenceCombineMode';
+
+export const OPERATOR_PARAM_OUTPUT_MAX = 'm_vecOutputMax';
+export const OPERATOR_PARAM_OUTPUT_MIN = 'm_vecOutputMin';
