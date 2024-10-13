c9067e001d3b8c952996fe6414320512427d3ca3
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-13 22:30:05 +0200

    parse() added return types

--- src/sourceengine/common/loaders/sourcebinaryloader.ts
+++ src/sourceengine/common/loaders/sourcebinaryloader.ts
@@ -1,5 +1,11 @@
 import { Repositories } from '../../../misc/repositories';
 import { customFetch } from '../../../utils/customfetch';
+import { SourceBSP } from '../../source1/loaders/sourcebsp';
+import { SourceMDL } from '../../source1/loaders/sourcemdl';
+import { SourcePCF } from '../../source1/loaders/sourcepcf';
+import { SourceVTX } from '../../source1/loaders/sourcevtx';
+import { SourceVVD } from '../../source1/loaders/sourcevvd';
+import { SourceEngineVTF } from '../../source1/textures/sourceenginevtf';
 import { Source2File } from '../../source2/loaders/source2file';
 
 export class SourceBinaryLoader {
@@ -43,7 +49,7 @@ export class SourceBinaryLoader {
 		return null;
 	}
 
-	parse(repository: string, fileName: string, arrayBuffer: ArrayBuffer): Promise<Source2File | any> {
+	parse(repository: string, fileName: string, arrayBuffer: ArrayBuffer): Promise<Source2File | any> | SourceVVD | SourceVTX | SourceEngineVTF | SourcePCF | SourceMDL | SourceBSP {
 		throw 'override me';
 	}
 }
