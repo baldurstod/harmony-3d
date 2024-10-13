e4e7891f7c89d1757a601e6aa352c7aebec02eed
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-13 22:09:43 +0200

    Added Source2SpriteCard

---
+++ src/sourceengine/source2/materials/export.ts
@@ -0,0 +1,3 @@
+
+export { Source2Material } from './source2material';
+export { Source2SpriteCard } from './source2spritecard';
---
+++ src/sourceengine/source2/materials/source2spritecard.ts
@@ -0,0 +1,73 @@
+import { vec2, vec3 } from 'gl-matrix';
+
+import { Source2Material } from './source2material';
+import { Source2MaterialLoader } from '../loaders/source2materialloader';
+import { Source2TextureManager } from '../textures/source2texturemanager';
+import { GL_ONE, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_DST_ALPHA } from '../../../webgl/constants';
+import { RenderFace } from '../../../materials/constants';
+import { Source2File } from '../loaders/source2file';
+
+export class Source2SpriteCard extends Source2Material {
+	#texturePath;
+	constructor(repository: string, source2File?: Source2File) {
+		super(repository, source2File);
+
+		//TODO: we should adapt transparency depending on particle renderer params ?
+		this.setTransparency(GL_SRC_ALPHA, GL_DST_ALPHA);
+		//this.setTransparency(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		this.setTransparency(GL_SRC_ALPHA, GL_ONE);
+		this.renderFace(RenderFace.Both);
+		this.setUniform('uFinalTextureScale', vec2.fromValues(1, 1));
+		this.setUniform('uColorScale', vec3.fromValues(1, 1, 1));
+
+		//this.setTransparency( GL_SRC_ALPHA, GL_ONE);
+		this.setTransparency(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+	}
+
+	setOutputBlendMode(outputBlendMode) {
+		switch (outputBlendMode) {
+			case 0:
+				this.setTransparency(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+				break;
+			case 1://PARTICLE_OUTPUT_BLEND_MODE_ADD
+				this.setTransparency(GL_SRC_ALPHA, GL_ONE);
+				break;
+			case 2://PARTICLE_OUTPUT_BLEND_MODE_BLEND_ADD //TODO ?
+				this.setTransparency(GL_SRC_ALPHA, GL_ONE);
+				break;
+			case 3://PARTICLE_OUTPUT_BLEND_MODE_HALF_BLEND_ADD //TODO ?
+				this.setTransparency(GL_SRC_ALPHA, GL_ONE);
+				break;
+			case 4://PARTICLE_OUTPUT_BLEND_MODE_NEG_HALF_BLEND_ADD //TODO ?
+				this.setTransparency(GL_SRC_ALPHA, GL_ONE);
+				break;
+			case 5://PARTICLE_OUTPUT_BLEND_MODE_MOD2X
+				this.setTransparency(GL_SRC_ALPHA, GL_DST_ALPHA);
+				break;
+			case 5://PARTICLE_OUTPUT_BLEND_MODE_LIGHTEN //TODO ?
+				this.setTransparency(GL_SRC_ALPHA, GL_ONE);
+				break;
+		}
+	}
+
+	async setTexturePath(texturePath) {
+		this.#texturePath = texturePath;
+	}
+
+	async initTextureUniforms() {
+		await super.initTextureUniforms();
+		if (this.#texturePath) {
+			this.setTexture('colorMap', await Source2TextureManager.getTexture(this.repository, this.#texturePath, 0), 'USE_COLOR_MAP');
+		}
+	}
+	getFrameSpan(sequence): number {
+		console.error('code me');
+		return 0;
+	}
+
+
+	getShaderSource() {
+		return 'source2_spritecard';
+	}
+}
+Source2MaterialLoader.registerMaterial('spritecard.vfx', Source2SpriteCard);
