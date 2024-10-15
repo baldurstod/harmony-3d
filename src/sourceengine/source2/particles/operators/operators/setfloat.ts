47233130d1aa7afdfc199924dde4a5ed06a444c0
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-15 21:13:52 +0200

    Added SetFloat

---
+++ src/sourceengine/source2/particles/operators/operators/setfloat.ts
@@ -0,0 +1,33 @@
+import { RegisterSource2ParticleOperator } from '../source2particleoperators';
+import { Operator } from '../operator';
+import { PARTICLE_FIELD_RADIUS } from '../../../../common/particles/particlefields';
+
+export class SetFloat extends Operator {
+	normalizePerLiving = true;
+	outputField = PARTICLE_FIELD_RADIUS;
+	setMethod = 'PARTICLE_SET_VALUE';
+
+	_paramChanged(paramName, value) {
+		switch (paramName) {
+			case 'm_InputValue':
+				break;
+			case 'm_nOutputField':
+				this.outputField = Number(value);
+				break;
+			case 'm_nSetMethod':
+				this.setMethod = value;
+				break;
+			default:
+				super._paramChanged(paramName, value);
+		}
+	}
+
+	doOperate(particle, elapsedTime) {
+		//TODO: use lerp
+		let value = this.getParamScalarValue('m_InputValue', particle);
+		//TODO: use setMethod
+		particle.setField(this.outputField, value, this.setMethod == 'PARTICLE_SET_SCALE_INITIAL_VALUE');
+		//particle.setField(this.outputField, value, this.setMethod == 'PARTICLE_SET_SCALE_INITIAL_VALUE');
+	}
+}
+RegisterSource2ParticleOperator('C_OP_SetFloat', SetFloat);
