cb8103111afcb40796a99bc5e1c69693d1205128
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-15 21:13:19 +0200

    Added SetControlPointsToModelParticles

---
+++ src/sourceengine/source2/particles/operators/operators/setcontrolpointstomodelparticles.ts
@@ -0,0 +1,74 @@
+import { RegisterSource2ParticleOperator } from '../source2particleoperators';
+import { Operator } from '../operator';
+
+export class SetControlPointsToModelParticles extends Operator {
+	#followAttachement = false;
+	#attachmentName = '';
+	hitboxSetName = 'default';
+	firstControlPoint = 0;
+	numControlPoints = 1;
+	firstSourcePoint = 0;
+	skin = false;
+
+	_paramChanged(paramName, value) {
+		switch (paramName) {
+			case 'm_HitboxSetName':
+				this.hitboxSetName = value;
+				break;
+			case 'm_AttachmentName':
+				this.#attachmentName = value;
+				break;
+			case 'm_nFirstControlPoint':
+				this.firstControlPoint = Number(value);
+				break;
+			case 'm_nNumControlPoints':
+				this.numControlPoints = Number(value);
+				break;
+			case 'm_nFirstSourcePoint':
+				this.firstSourcePoint = Number(value);
+				break;
+			case 'm_bSkin':
+				this.skin = value;
+				break;
+			case 'm_bAttachment':
+				this.#followAttachement = value;
+				break;
+			default:
+				super._paramChanged(paramName, value);
+		}
+	}
+
+	doOperate(particle, elapsedTime) {
+		//todo: use m_bSkin m_bAttachment m_HitboxSetName m_AttachmentName
+		let children = this.system.childSystems;
+		let firstControlPoint = this.firstControlPoint;
+		let firstSourcePoint = this.firstSourcePoint;
+
+
+		for (let i = 0; i < this.numControlPoints; ++i) {
+			let particle = this.system.livingParticles[firstSourcePoint + i];
+			if (particle) {
+				for (let child of children) {
+					let childCp = child.getOwnControlPoint(firstControlPoint + i);
+					childCp.position = particle.position;
+
+					if (this.#followAttachement) {
+						const model = this.system.getParentModel();
+						if (model) {
+							const attachement = model.getAttachement(this.#attachmentName);
+							if (attachement) {
+								childCp.quaternion = attachement.getWorldQuaternion();
+								childCp.quaternion = particle.quaternion;
+							}
+						}
+					}
+					if (childCp.lastComputed == -1) {
+						// Discard the delta if it's a newly create control point
+						childCp.resetDelta();
+					}
+				}
+			}
+		}
+	}
+}
+RegisterSource2ParticleOperator('C_OP_SetControlPointsToModelParticles', SetControlPointsToModelParticles);
