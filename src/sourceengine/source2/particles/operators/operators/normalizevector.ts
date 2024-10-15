2969e2f487007da2d9da75e5dbbeac90afb2c678
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-15 20:33:30 +0200

    Added NormalizeVector

---
+++ src/sourceengine/source2/particles/operators/operators/normalizevector.ts
@@ -0,0 +1,29 @@
+import { vec3 } from 'gl-matrix';
+import { RegisterSource2ParticleOperator } from '../source2particleoperators';
+import { Operator } from '../operator';
+import { PARTICLE_FIELD_POSITION } from '../../../../common/particles/particlefields';
+
+let v = vec3.create();
+
+export class NormalizeVector extends Operator {
+	fieldOutput = PARTICLE_FIELD_POSITION;
+	scale = 1;
+
+	_paramChanged(paramName, value) {
+		switch (paramName) {
+			case 'm_flScale':
+				this.scale = value;
+				break;
+			default:
+				super._paramChanged(paramName, value);
+		}
+	}
+
+	doOperate(particle, elapsedTime) {
+		vec3.copy(v, particle.getField(this.fieldOutput));
+		vec3.normalize(v, v);
+		vec3.scale(v, v, this.scale);
+		particle.setField(this.fieldOutput, v);
+	}
+}
+RegisterSource2ParticleOperator('C_OP_NormalizeVector', NormalizeVector);
