df2dc7cc201bafcfea1cc5f2a81d363a2d5c539c
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-15 21:12:38 +0200

    Added SetControlPointFromObjectScale

---
+++ src/sourceengine/source2/particles/operators/operators/setcontrolpointfromobjectscale.ts
@@ -0,0 +1,34 @@
+import { vec3 } from 'gl-matrix';
+import { RegisterSource2ParticleOperator } from '../source2particleoperators';
+import { Operator } from '../operator';
+
+export class SetControlPointFromObjectScale extends Operator {
+	cpInput = 0;
+	cpOutput = 1;
+
+	_paramChanged(paramName, value) {
+		switch (paramName) {
+			case 'm_nCPInput':
+				this.cpInput  = Number(value);
+				break;
+			case 'm_nCPOutput':
+				this.cpOutput  = Number(value);
+				break;
+			default:
+				super._paramChanged(paramName, value);
+		}
+	}
+
+	doOperate(particle, elapsedTime) {
+		let cpInput = this.system.getControlPoint(this.cpInput);
+		let cpOutput = this.system.getControlPoint(this.cpOutput);
+
+		//TODO: use the actual scale
+		cpOutput.position = vec3.fromValues(1, 1, 1);
+	}
+
+	isPreEmission() {
+		return true;
+	}
+}
+RegisterSource2ParticleOperator('C_OP_SetControlPointFromObjectScale', SetControlPointFromObjectScale);
