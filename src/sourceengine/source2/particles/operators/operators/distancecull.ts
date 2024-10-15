a90b9b59ed6457346ba960cd180753832cc30293
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-15 15:07:12 +0200

    Added DistanceCull

---
+++ src/sourceengine/source2/particles/operators/operators/distancecull.ts
@@ -0,0 +1,49 @@
+import { vec3 } from 'gl-matrix';
+import { RegisterSource2ParticleOperator } from '../source2particleoperators';
+import { Operator } from '../operator';
+
+let vec = vec3.create();
+
+export class DistanceCull extends Operator {
+	pointOffset = vec3.create();
+	distance = 0;
+	cullInside = false;
+
+	_paramChanged(paramName, value) {
+		switch (paramName) {
+			case 'm_nControlPoint':
+				this.controlPointNumber = Number(value);
+				break;
+			case 'm_vecPointOffset':
+				vec3.copy(this.pointOffset, value);
+				break;
+			case 'm_flDistance':
+				this.distance = value;
+				break;
+			case 'm_bCullInside':
+				this.cullInside = value;
+				break;
+			default:
+				super._paramChanged(paramName, value);
+		}
+	}
+
+	doOperate(particle, elapsedTime) {
+		let cp = this.system.getControlPoint(this.controlPointNumber);
+		if (cp) {
+			let origin = cp.getWorldPosition(vec);
+			vec3.add(origin, origin, this.pointOffset);
+
+			if (this.cullInside) {//TODO: improve this
+				if (vec3.distance(particle.position, origin) < this.distance) {
+					particle.die();
+				}
+			} else {
+				if (vec3.distance(particle.position, origin) > this.distance) {
+					particle.die();
+				}
+			}
+		}
+	}
+}
+RegisterSource2ParticleOperator('C_OP_DistanceCull', DistanceCull);
