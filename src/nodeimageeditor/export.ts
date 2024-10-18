f427bf988d9950e9ff713b5c3fdb0207cc825ede
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-18 11:52:32 +0200

    Export Node

--- src/nodeimageeditor/export.ts
+++ src/nodeimageeditor/export.ts
@@ -4,5 +4,6 @@ export * from './nodes/export';
 export * from './operations/export';
 export * from './shaders/export';
 
+export { Node } from './node';
 export { NodeImageEditor, DEFAULT_TEXTURE_SIZE } from './nodeimageeditor';
 export { NodeImageEditorMaterial } from './nodeimageeditormaterial';
