3ec8cdf2d4f9f1deffcf48be122f94340dad0c9f
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-22 23:32:13 +0200

    Added declare_vertex_uv2.glsl

---
+++ src/shaders/includes/core/uv/declare_vertex_uv2.glsl
@@ -0,0 +1,5 @@
+export default `
+#ifdef USE_TEXTURE2_TRANSFORM
+	uniform mat4 uTexture2Transform;
+#endif
+`;
---
+++ src/shaders/includes/core/uv/export.js
@@ -0,0 +1,4 @@
+import { Includes } from '../../../../shaders/includes.js';
+
+import declare_vertex_uv2 from './declare_vertex_uv2.glsl';
+Includes['declare_vertex_uv2'] = declare_vertex_uv2;
