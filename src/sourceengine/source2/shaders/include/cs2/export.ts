59b4bb8322ce27ff09d9a63494703c2e23091964
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-22 23:39:07 +0200

    Added source2_varying_csgo_weapon_stattrak.glsl

---
+++ src/sourceengine/source2/shaders/include/cs2/export.ts
@@ -0,0 +1,4 @@
+import { Includes } from '../../../../../shaders/includes';
+
+import source2_varying_csgo_weapon_stattrak from './source2_varying_csgo_weapon_stattrak.glsl';
+Includes['source2_varying_csgo_weapon_stattrak'] = source2_varying_csgo_weapon_stattrak;
