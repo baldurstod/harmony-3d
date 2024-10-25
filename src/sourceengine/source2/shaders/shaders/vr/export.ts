7750b52260aadb933774a733aa66453e768bb187
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-25 12:59:10 +0200

    Added source2_vr_complex shader

--- src/sourceengine/source2/shaders/shaders/vr/export.ts
+++ src/sourceengine/source2/shaders/shaders/vr/export.ts
@@ -4,3 +4,8 @@ import source2_vr_black_unlit_fs from './source2_vr_black_unlit.fs';
 import source2_vr_black_unlit_vs from './source2_vr_black_unlit.vs';
 Shaders['source2_vr_black_unlit.fs'] = source2_vr_black_unlit_fs;
 Shaders['source2_vr_black_unlit.vs'] = source2_vr_black_unlit_vs;
+
+import source2_vr_complex_fs from './source2_vr_complex.fs';
+import source2_vr_complex_vs from './source2_vr_complex.vs';
+Shaders['source2_vr_complex.fs'] = source2_vr_complex_fs;
+Shaders['source2_vr_complex.vs'] = source2_vr_complex_vs;
