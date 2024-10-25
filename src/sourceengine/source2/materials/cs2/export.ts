12288af152c23749f6a27b4f001e33cf9483baa0
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-25 12:16:46 +0200

    Added material csgo_effects.vfx

--- src/sourceengine/source2/materials/cs2/export.ts
+++ src/sourceengine/source2/materials/cs2/export.ts
@@ -1,4 +1,5 @@
 export { Source2CsgoCharacter } from './character';
 export { Source2CsgoComplex } from './complex';
+export { Source2CsgoEffects } from './effects';
 export { Source2CsgoEnvironmentBlend } from './environment_blend';
 export { Source2CsgoWeaponStattrak } from './weapon_stattrak';
