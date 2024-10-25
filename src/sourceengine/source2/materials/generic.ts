3b029d11ace5949da767c7e6638d240ece35109e
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-25 12:41:50 +0200

    Added material generic.vfx

---
+++ src/sourceengine/source2/materials/generic.ts
@@ -0,0 +1,10 @@
+import { Source2Material } from './source2material';
+import { Source2MaterialLoader } from '../loaders/source2materialloader';
+
+export class Source2Generic extends Source2Material{
+
+	get shaderSource() {
+		return 'source2_hero';//TODO: code proper shader
+	}
+}
+Source2MaterialLoader.registerMaterial('generic.vfx', Source2Generic);
