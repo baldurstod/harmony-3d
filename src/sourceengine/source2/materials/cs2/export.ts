3ceb71b7998c1868726e5bd248a72eb7092a791f
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-25 12:19:33 +0200

    Added material csgo_glass.vfx

--- src/sourceengine/source2/materials/cs2/export.ts
+++ src/sourceengine/source2/materials/cs2/export.ts
@@ -4,4 +4,5 @@ export { Source2CsgoEffects } from './effects';
 export { Source2CsgoEnvironmentBlend } from './environment_blend';
 export { Source2CsgoEnvironment } from './environment';
 export { Source2CsgoFoliage } from './foliage';
+export { Source2CsgoGlass } from './glass';
 export { Source2CsgoWeaponStattrak } from './weapon_stattrak';
---
+++ src/sourceengine/source2/materials/cs2/glass.ts
@@ -0,0 +1,10 @@
+import { Source2Material } from '../source2material';
+import { Source2MaterialLoader } from '../../loaders/source2materialloader';
+
+export class Source2CsgoGlass extends Source2Material{
+
+	get shaderSource() {
+		return 'source2_vr_simple';//TODO: code proper shader
+	}
+}
+Source2MaterialLoader.registerMaterial('csgo_glass.vfx', Source2CsgoGlass);
