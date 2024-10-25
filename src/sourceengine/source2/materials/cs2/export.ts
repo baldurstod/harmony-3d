579112d74e2cb60644ee69e679e3bdeac1bd41ee
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-25 12:18:11 +0200

    Added material csgo_foliage.vfx

--- src/sourceengine/source2/materials/cs2/export.ts
+++ src/sourceengine/source2/materials/cs2/export.ts
@@ -3,4 +3,5 @@ export { Source2CsgoComplex } from './complex';
 export { Source2CsgoEffects } from './effects';
 export { Source2CsgoEnvironmentBlend } from './environment_blend';
 export { Source2CsgoEnvironment } from './environment';
+export { Source2CsgoFoliage } from './foliage';
 export { Source2CsgoWeaponStattrak } from './weapon_stattrak';
---
+++ src/sourceengine/source2/materials/cs2/foliage.ts
@@ -0,0 +1,10 @@
+import { Source2Material } from '../source2material';
+import { Source2MaterialLoader } from '../../loaders/source2materialloader';
+
+export class Source2CsgoFoliage extends Source2Material{
+
+	get shaderSource() {
+		return 'source2_vr_xen_foliage';//TODO: code proper shader
+	}
+}
+Source2MaterialLoader.registerMaterial('csgo_foliage.vfx', Source2CsgoFoliage);
