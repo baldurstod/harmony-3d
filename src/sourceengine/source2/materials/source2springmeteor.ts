605b6b5156f5a87e120530cecb101cce513e870e
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-25 12:42:58 +0200

    Added material spring_meteor.vfx

---
+++ src/sourceengine/source2/materials/source2springmeteor.ts
@@ -0,0 +1,10 @@
+import { Source2Material } from './source2material';
+import { Source2MaterialLoader } from '../loaders/source2materialloader';
+
+export class Source2SpringMeteor extends Source2Material {
+
+	get shaderSource() {
+		return 'source2_spring_meteor';
+	}
+}
+Source2MaterialLoader.registerMaterial('spring_meteor.vfx', Source2SpringMeteor);
