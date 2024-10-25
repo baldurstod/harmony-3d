8642ed0a88b1d89297b22c1b9d7404c32700f39e
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-25 13:08:03 +0200

    Added source2 vr includes

--- src/sourceengine/source2/shaders/include/vr/varying/export.ts
+++ src/sourceengine/source2/shaders/include/vr/varying/export.ts
@@ -2,3 +2,24 @@ import { Includes } from '../../../../../../shaders/includes';
 
 import source2_varying_vr_black_unlit from './source2_varying_vr_black_unlit.glsl';
 Includes['source2_varying_vr_black_unlit'] = source2_varying_vr_black_unlit;
+
+import source2_varying_vr_complex from './source2_varying_vr_complex.glsl';
+Includes['source2_varying_vr_complex'] = source2_varying_vr_complex;
+
+import source2_varying_vr_eyeball from './source2_varying_vr_eyeball.glsl';
+Includes['source2_varying_vr_eyeball'] = source2_varying_vr_eyeball;
+
+import source2_varying_vr_glass from './source2_varying_vr_glass.glsl';
+Includes['source2_varying_vr_glass'] = source2_varying_vr_glass;
+
+import source2_varying_vr_simple_3layer_parallax from './source2_varying_vr_simple_3layer_parallax.glsl';
+Includes['source2_varying_vr_simple_3layer_parallax'] = source2_varying_vr_simple_3layer_parallax;
+
+import source2_varying_vr_simple from './source2_varying_vr_simple.glsl';
+Includes['source2_varying_vr_simple'] = source2_varying_vr_simple;
+
+import source2_varying_vr_skin from './source2_varying_vr_skin.glsl';
+Includes['source2_varying_vr_skin'] = source2_varying_vr_skin;
+
+import source2_varying_vr_xen_foliage from './source2_varying_vr_xen_foliage.glsl';
+Includes['source2_varying_vr_xen_foliage'] = source2_varying_vr_xen_foliage;
