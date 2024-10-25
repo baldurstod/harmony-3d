8519aec63e54c58580adc5b7c6472261cdeb976b
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-25 12:32:14 +0200

    Added material vr_monitor.vfx

--- src/sourceengine/source2/materials/vr/export.ts
+++ src/sourceengine/source2/materials/vr/export.ts
@@ -2,3 +2,4 @@ export { Source2VrBlackUnlit } from './source2vrblackunlit';
 export { Source2VrComplex } from './source2vrcomplex';
 export { Source2VrEyeball } from './source2vreyeball';
 export { Source2VrGlass } from './source2vrglass';
+export { Source2VrMonitor } from './source2vrmonitor';
---
+++ src/sourceengine/source2/materials/vr/source2vrmonitor.ts
@@ -0,0 +1,10 @@
+import { Source2Material } from '../source2material';
+import { Source2MaterialLoader } from '../../loaders/source2materialloader';
+
+export class Source2VrMonitor extends Source2Material {
+
+	get shaderSource() {
+		return 'source2_vr_monitor';
+	}
+}
+Source2MaterialLoader.registerMaterial('vr_monitor.vfx', Source2VrMonitor);
