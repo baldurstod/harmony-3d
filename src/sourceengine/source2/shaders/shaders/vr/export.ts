68304a9f17aac32e2fe7f6aa8fb8ed3497571fd1
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-25 13:00:12 +0200

    Added source2_vr_eyeball shader

--- src/sourceengine/source2/shaders/shaders/vr/export.ts
+++ src/sourceengine/source2/shaders/shaders/vr/export.ts
@@ -9,3 +9,8 @@ import source2_vr_complex_fs from './source2_vr_complex.fs';
 import source2_vr_complex_vs from './source2_vr_complex.vs';
 Shaders['source2_vr_complex.fs'] = source2_vr_complex_fs;
 Shaders['source2_vr_complex.vs'] = source2_vr_complex_vs;
+
+import source2_vr_eyeball_fs from './source2_vr_eyeball.fs';
+import source2_vr_eyeball_vs from './source2_vr_eyeball.vs';
+Shaders['source2_vr_eyeball.fs'] = source2_vr_eyeball_fs;
+Shaders['source2_vr_eyeball.vs'] = source2_vr_eyeball_vs;
