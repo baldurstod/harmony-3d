b03d033ce8a195830895f0335b92b4478ae40fcd
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-25 12:31:42 +0200

    Added material vr_glass.vfx

--- src/sourceengine/source2/materials/vr/export.ts
+++ src/sourceengine/source2/materials/vr/export.ts
@@ -1,3 +1,4 @@
 export { Source2VrBlackUnlit } from './source2vrblackunlit';
 export { Source2VrComplex } from './source2vrcomplex';
 export { Source2VrEyeball } from './source2vreyeball';
+export { Source2VrGlass } from './source2vrglass';
---
+++ src/sourceengine/source2/materials/vr/source2vrglass.ts
@@ -0,0 +1,10 @@
+import { Source2Material } from '../source2material';
+import { Source2MaterialLoader } from '../../loaders/source2materialloader';
+
+export class Source2VrGlass extends Source2Material{
+
+	get shaderSource() {
+		return 'source2_vr_glass';
+	}
+}
+Source2MaterialLoader.registerMaterial('vr_glass.vfx', Source2VrGlass);
