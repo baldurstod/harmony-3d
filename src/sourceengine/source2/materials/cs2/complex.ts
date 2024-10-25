c7a379b260b7b693fab41b8015d3e1bddbd78172
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-25 12:15:21 +0200

    Added csgo_complex.vfx

---
+++ src/sourceengine/source2/materials/cs2/complex.ts
@@ -0,0 +1,10 @@
+import { Source2Material } from '../source2material';
+import { Source2MaterialLoader } from '../../loaders/source2materialloader';
+
+export class Source2CsgoComplex extends Source2Material{
+
+	get shaderSource() {
+		return 'source2_hero';//TODO: code proper shader
+	}
+}
+Source2MaterialLoader.registerMaterial('csgo_complex.vfx', Source2CsgoComplex);
--- src/sourceengine/source2/materials/cs2/export.ts
+++ src/sourceengine/source2/materials/cs2/export.ts
@@ -1,3 +1,4 @@
 export { Source2CsgoCharacter } from './character';
+export { Source2CsgoComplex } from './complex';
 export { Source2CsgoEnvironmentBlend } from './environment_blend';
 export { Source2CsgoWeaponStattrak } from './weapon_stattrak';
