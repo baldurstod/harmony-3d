cb2dc2c57fafe4ce939eab0fd3d023a112110245
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-11-09 22:50:11 +0100

    Removed dead code

--- src/sourceengine/common/loaders/sourcebinaryloader.ts
+++ src/sourceengine/common/loaders/sourcebinaryloader.ts
@@ -13,14 +13,6 @@ export class SourceBinaryLoader {
 	async load(repositoryName: string, fileName: string): Promise<Source2File | any> {
 		this.repository = repositoryName;
 
-		/*
-		const repository = new Repositories().getRepository(repositoryName);
-		if (!repository) {
-			console.error(`Unknown repository ${repositoryName} in SourceBinaryLoader.load`);
-			return null;
-		}
-			*/
-
 		let promise = new Promise<Source2File | any>(resolve => {
 			const p = new Repositories().getFile(repositoryName, fileName);
 			p.then((arrayBuffer) => {
@@ -30,38 +22,9 @@ export class SourceBinaryLoader {
 					resolve(null);
 				}
 			});
-			/*
-			customFetch(new URL(fileName, repository.base)).then(
-				async response => {
-					if (response?.ok) {
-						resolve(this.parse(repositoryName, fileName, await response.arrayBuffer()));
-					} else {
-						resolve(null);
-					}
-				},
-				(error) => { console.error(error); resolve(null); }
-			);
-			*/
 		});
 		return promise;
 	}
-	/*
-	async load2(repositoryName, fileName) {
-		this.repository = repositoryName;
-		const repository = new Repositories().getRepository(repositoryName);
-		if (!repository) {
-			console.error(`Unknown repository ${repositoryName} in SourceBinaryLoader.load2`);
-			return null;
-		}
-
-		const response = await customFetch(new URL(fileName, repository.base));
-
-		if (response.ok) {
-			return this.parse(repositoryName, fileName, await response.arrayBuffer());
-		}
-		return null;
-	}
-	*/
 
 	parse(repository: string, fileName: string, arrayBuffer: ArrayBuffer): Promise<Source2File | any> | SourceVVD | SourceVTX | SourceEngineVTF | SourcePCF | SourceMDL | SourceBSP {
 		throw 'override me';
