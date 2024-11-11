fbf333e7c7d30fbf14745e2c1d1508fea21e5a62
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-11-11 21:34:11 +0100

    Change RepositoryEntry from type to class

--- src/repositories/repository.ts
+++ src/repositories/repository.ts
@@ -12,7 +12,7 @@ export type RepositoryFileListResponse = { root?: RepositoryEntry, error?: Repos
 
 export type RepositoryFilter = { extension?: string };
 
-export type RepositoryEntry = { name: string, childs?: Array<RepositoryEntry>, directory?: boolean };
+//export type RepositoryEntry = { name: string, childs?: Array<RepositoryEntry>, directory?: boolean };
 
 export interface Repository {
 	name: string;
@@ -21,4 +21,51 @@ export interface Repository {
 	getFileAsBlob: (filepath: string) => Promise<RepositoryBlobResponse>;
 	getFileAsJson: (filepath: string) => Promise<RepositoryJsonResponse>;
 	getFileList: (filter?: RepositoryFilter) => Promise<RepositoryFileListResponse>;
+}
+
+class RepositoryEntry {
+	#name: string;
+	#childs = new Map<string, RepositoryEntry>;
+	#isDirectory: boolean;
+
+	constructor(name: string, isDirectory: boolean) {
+		this.#name = name;
+		this.#isDirectory = isDirectory;
+	}
+
+	addEntry(filename: string) {
+		const splittedPath = filename.split(/[\/\\]+/);
+		let current: RepositoryEntry = this;
+		let len = splittedPath.length - 1;
+
+		for (const [i, p] of splittedPath.entries()) {
+			if (!current.#childs.has(p)) {
+				current = current.#addFile(p, i != len);
+			} else {
+				current = current.#childs.get(p) as RepositoryEntry;
+			}
+		}
+	}
+
+	#addFile(name: string, isDirectory: boolean) {
+		const e = new RepositoryEntry(name, isDirectory);
+		this.#childs.set(name, e);
+		return e;
+	}
+
+	getChild(name: string): RepositoryEntry | undefined {
+		return this.#childs.get(name);
+	}
+
+	toJSON() {
+		const json: any/*TODO:improve type*/ = { name: this.#name };
+		if (this.#isDirectory) {
+			const files: Array<any/*TODO:improve type*/> = [];
+			for (const [_, child] of this.#childs) {
+				files.push(child.toJSON());
+			}
+			json.files = files;
+		}
+		return json;
+	}
 }
