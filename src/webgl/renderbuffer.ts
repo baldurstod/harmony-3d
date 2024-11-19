456a6f82fda8a250db96f408fef092190cbf9576
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-11-19 18:51:32 +0100

    Added typescript declarations

--- src/webgl/renderbuffer.ts
+++ src/webgl/renderbuffer.ts
@@ -2,8 +2,9 @@ import { GL_RENDERBUFFER } from './constants';
 import { Graphics } from '../graphics/graphics';
 import { DEBUG, ENABLE_GET_ERROR } from '../buildoptions';
 import { RenderBufferInternalFormat } from '../textures/constants';
+import { WebGLAnyRenderingContext } from '../types';
 
-function renderbufferStorage(glContext, renderbuffer, internalFormat, width, height) {
+function renderbufferStorage(glContext: WebGLAnyRenderingContext, renderbuffer: WebGLRenderbuffer, internalFormat: RenderBufferInternalFormat, width: number, height: number) {
 	if (ENABLE_GET_ERROR && DEBUG) {
 		new Graphics().cleanupGLError();
 	}
@@ -22,7 +23,7 @@ export class Renderbuffer {
 	#renderbuffer: WebGLRenderbuffer;
 	#internalFormat: RenderBufferInternalFormat;
 	constructor(internalFormat: RenderBufferInternalFormat, width: number, height: number) {
-		this.#renderbuffer = new Graphics().createRenderbuffer();
+		this.#renderbuffer = new Graphics().createRenderbuffer() as WebGLRenderbuffer;
 		this.#internalFormat = internalFormat;
 		//this._renderbufferStorage(width, height);
 		renderbufferStorage(new Graphics().glContext, this.#renderbuffer, this.#internalFormat, width, height);
