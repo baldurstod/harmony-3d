5ace96957997663f813fef7f608ab4e6b970b453
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-11-23 16:57:45 +0100

    TimelineChannel: added clips

--- src/timeline/channel.ts
+++ src/timeline/channel.ts
@@ -1,10 +1,20 @@
+import { TimelineClip } from './clip';
 import { TimelineElement, TimelineElementType } from './element';
 
 export class TimelineChannel extends TimelineElement {
 	type: TimelineElementType.Channel = TimelineElementType.Channel;
+	#clips = new Set<TimelineClip>;
 
 	constructor(name: string = 'Channel') {
 		super(name);
+	}
 
+	addClip(clip: TimelineClip) {
+		this.#clips.add(clip);
+		return clip;
+	}
+
+	getChilds() {
+		return [...this.#clips];
 	}
 }
