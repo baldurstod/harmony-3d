0a2cbafc41c0d451cedba7aa516ae026f1d9a6b4
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-12-01 23:35:32 +0100

    Added typescript declarations

--- src/materials/materialmanager.ts
+++ src/materials/materialmanager.ts
@@ -1,9 +1,10 @@
 import { TESTING } from '../buildoptions';
+import { Material } from './material';
 
 export class MaterialManager {
-	static #materials = new Map();
+	static #materials = new Map<string, { materialClass: typeof Material, manager: any/*TODO: better type*/ }>();
 
-	static registerMaterial(materialName, materialClass, manager) {
+	static registerMaterial(materialName: string, materialClass: typeof Material, manager: any/*TODO: better type*/) {
 		if (TESTING) {
 			if (!materialName) {
 				throw 'Missing material name';
@@ -11,11 +12,14 @@ export class MaterialManager {
 			if (!materialClass) {
 				throw 'Missing material class';
 			}
+			if (manager) {
+				throw 'Remove this parameter';
+			}
 		}
 		this.#materials.set(materialName, { materialClass: materialClass, manager: manager });
 	}
 
-	static getMaterial(materialName, callback) {
+	static getMaterial(materialName: string, callback: (material: Material) => void) {
 		let material = this.#materials.get(materialName);
 		if (material) {
 			let manager = material.manager;
