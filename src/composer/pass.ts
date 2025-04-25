570330d106cd0af8397c551b3530b3a1344cb28e
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-04-25 09:40:25 +0200

    Added strict typescript declarations

--- src/composer/pass.ts
+++ src/composer/pass.ts
@@ -1,18 +1,20 @@
 import { Camera } from '../cameras/camera';
+import { Graphics } from '../graphics/graphics';
 import { FullScreenQuad } from '../primitives/fullscreenquad';
+import { RenderTarget } from '../textures/rendertarget';
 
 export class Pass {
-	camera: Camera;
+	camera?: Camera;
 	quad?: FullScreenQuad;
 	enabled = true;
 	swapBuffers = true;
 	renderToScreen = false;
 
-	setSize(width, height) {
+	setSize(width: number, height: number) {
 
 	}
 
-	render(renderer, readBuffer, writeBuffer, renderToScreen, delta) {
+	render(renderer: Graphics, readBuffer: RenderTarget, writeBuffer: RenderTarget, renderToScreen: boolean, delta: number) {
 		throw 'Can\'t render default pass';
 	}
 }
