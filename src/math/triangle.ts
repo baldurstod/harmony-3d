e814569a553ba12850b248ef9ef5524285e24366
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-04-29 23:12:48 +0200

    Added typescript declarations

--- src/math/triangle.ts
+++ src/math/triangle.ts
@@ -1,11 +1,11 @@
-import { vec3 } from 'gl-matrix';
+import { vec2, vec3 } from 'gl-matrix';
 
 let v0 = vec3.create();
 let v1 = vec3.create();
 let v2 = vec3.create();
 let v3 = vec3.create();
 
-export function getBarycentricCoordinates(out, position, a, b, c) {
+export function getBarycentricCoordinates(out: vec3, position: vec3, a: vec3, b: vec3, c: vec3) {
 	vec3.sub(v0, c, a);
 	vec3.sub(v1, b, a);
 	vec3.sub(v2, position, a);
@@ -17,14 +17,14 @@ export function getBarycentricCoordinates(out, position, a, b, c) {
 	let dot12 = vec3.dot(v1, v2);
 
 	let invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
-	let u = ( dot11 * dot02 - dot01 * dot12 ) * invDenom;
-	let v = ( dot00 * dot12 - dot01 * dot02 ) * invDenom;
+	let u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+	let v = (dot00 * dot12 - dot01 * dot02) * invDenom;
 
 	// barycentric coordinates must always sum to 1
-	return vec3.set(out,  1 - u - v, v, u );
+	return vec3.set(out, 1 - u - v, v, u);
 }
 
-export function getUV(out, position, a, b, c, uv1, uv2, uv3) {
+export function getUV(out: vec3, position: vec3, a: vec3, b: vec3, c: vec3, uv1: vec2, uv2: vec2, uv3: vec2) {
 	getBarycentricCoordinates(v3, position, a, b, c);
 
 	out[0] = uv1[0] * v3[0] + uv2[0] * v3[1] + uv3[0] * v3[2];
@@ -32,7 +32,7 @@ export function getUV(out, position, a, b, c, uv1, uv2, uv3) {
 	return out;
 }
 
-export function getNormal(out, position, a, b, c, normal1, normal2, normal3) {
+export function getNormal(out: vec3, position: vec3, a: vec3, b: vec3, c: vec3, normal1: vec3, normal2: vec3, normal3: vec3) {
 	getBarycentricCoordinates(v3, position, a, b, c);
 
 	out[0] = normal1[0] * v3[0] + normal2[0] * v3[1] + normal3[0] * v3[2];
