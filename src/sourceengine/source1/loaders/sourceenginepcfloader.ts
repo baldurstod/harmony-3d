b6eac6d1cd0d018350fa430cf4200028589f5212
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-07-25 15:01:47 +0200

    Added strict typescript declarations

--- src/sourceengine/source1/loaders/sourceenginepcfloader.ts
+++ src/sourceengine/source1/loaders/sourceenginepcfloader.ts
@@ -1,27 +1,26 @@
 import { vec2, vec3, vec4 } from 'gl-matrix';
 import { BinaryReader } from 'harmony-binary-reader';
-
-import { SourceBinaryLoader } from '../../common/loaders/sourcebinaryloader';
-import { DmeParticleSystemDefinition, SourcePCF } from './sourcepcf';
-import { Color } from '../particles/color';
 import { registerLoader } from '../../../loaders/loaderfactory';
+import { SourceBinaryLoader } from '../../common/loaders/sourcebinaryloader';
+import { Color } from '../particles/color';
+import { DmeParticleSystemDefinition, SourcePCF } from './sourcepcf';
 
 const _PCF_LOADER_DEBUG_ = false;//TODOv3 remove
 
 
 const data_size = [
-0, 4, 4, 4, 1, 0, 0, 4, 4, 8, 12, 16, 12, 16, 64,
-4, 4, 4, 1, 0, 0, 4, 4, 8, 12, 16, 12, 16, 64,
+	0, 4, 4, 4, 1, 0, 0, 4, 4, 8, 12, 16, 12, 16, 64,
+	4, 4, 4, 1, 0, 0, 4, 4, 8, 12, 16, 12, 16, 64,
 ];
 
 const BINARY_FORMAT_POS = 25;
 
 export class SourceEnginePCFLoader extends SourceBinaryLoader {
-	parse(repositoryName, fileName, arrayBuffer) {
-		const pcf = new SourcePCF(repositoryName);
-		const reader = new BinaryReader(arrayBuffer);
+	parse(repository: string, path: string, content: ArrayBuffer): SourcePCF | null {
+		const pcf = new SourcePCF(repository, path);
+		const reader = new BinaryReader(content);
 
-		const str = reader.getString(Math.min(1000, arrayBuffer.byteLength));
+		const str = reader.getString(Math.min(1000, content.byteLength));
 
 		let startOffset = str.indexOf('-->');
 		if (startOffset === 0) {
@@ -38,12 +37,12 @@ export class SourceEnginePCFLoader extends SourceBinaryLoader {
 		return pcf;
 	}
 
-	#parseHeader(reader, pcf, startOffset) {
+	#parseHeader(reader: BinaryReader, pcf: SourcePCF, startOffset: number): void {
 		reader.seek(startOffset);
 
 		let nStrings = 0;
 		let nElements = 0
-		if (pcf.binaryVersion<5) {
+		if (pcf.binaryVersion < 5) {
 			nStrings = reader.getUint16();
 		} else {
 			nStrings = reader.getUint32();
@@ -53,31 +52,31 @@ export class SourceEnginePCFLoader extends SourceBinaryLoader {
 			pcf.stringDict.push(reader.getNullString());
 		}
 
-		if (pcf.binaryVersion<5) {
+		if (pcf.binaryVersion < 5) {
 			nElements = reader.getUint32();
 		} else {
 			nElements = reader.getUint32();
 		}
 
-		for (let i = 0; i < nElements; ++i) {
+		for (let i = 0; i < nElements; i++) {
 			pcf.elementsDict.push(this.#parseElement(reader, pcf));
 		}
 
-		for (let i=0; i<nElements; i++) {
-			pcf.elementsDict[i].attributes = this.#parseAttributes(reader, pcf);
+		for (let i = 0; i < nElements; i++) {
+			pcf.elementsDict[i]!.attributes = this.#parseAttributes(reader, pcf);
 		}
 	}
 
-	#parseElement(reader, pcf) {
-		const element = new CDmxElement();
+	#parseElement(reader: BinaryReader, pcf: SourcePCF): CDmxElement {
+		const element: Partial<CDmxElement> = {};
 
-		if (pcf.binaryVersion<5) {
+		if (pcf.binaryVersion < 5) {
 			element.type = this.getString(pcf, reader.getUint16());
 		} else {
 			element.type = this.getString(pcf, reader.getUint32());
 		}
 
-		if (pcf.binaryVersion<5) {
+		if (pcf.binaryVersion < 5) {
 			element.name = reader.getNullString();
 		} else {
 			element.name = this.getString(pcf, reader.getUint32())
@@ -85,17 +84,17 @@ export class SourceEnginePCFLoader extends SourceBinaryLoader {
 		element.guid = reader.getBytes(16);
 		element.guid2 = '';
 		for (let i = 0; i < 16; ++i) {
-			element.guid2 += String.fromCharCode(element.guid[i]);
+			element.guid2 += String.fromCharCode(element.guid[i]!);
 		}
 
 		if (element.type == DmeParticleSystemDefinition) {
 			pcf.addSystem(element);
 		}
-		return element;
+		return element as CDmxElement;
 	}
 
-	#parseAttributes(reader, pcf) {
-		const attributes = [];
+	#parseAttributes(reader: BinaryReader, pcf: SourcePCF): CDmxAttribute[] {
+		const attributes: CDmxAttribute[] = [];
 
 		const nAttributes = reader.getUint32();
 		for (let i = 0; i < nAttributes; ++i) {
@@ -105,29 +104,28 @@ export class SourceEnginePCFLoader extends SourceBinaryLoader {
 		return attributes;
 	}
 
-	#parseAttribute(reader, pcf) {
-		const attribute = new CDmxAttribute();
+	#parseAttribute(reader: BinaryReader, pcf: SourcePCF): CDmxAttribute {
+		const attribute: Partial<CDmxAttribute> = {};
 
-		if (pcf.binaryVersion<5) {
+		if (pcf.binaryVersion < 5) {
 			attribute.typeName = this.getString(pcf, reader.getUint16());
 		} else {
 			attribute.typeName = this.getString(pcf, reader.getUint32());
 		}
 
 		attribute.type = reader.getUint8()
-		attribute.value = null;
 
 		if (attribute.type > 14) {
 			attribute.value = this.#parseArray(reader, pcf, attribute.type);
 		} else {
 			attribute.value = this.#parseValue(reader, pcf, attribute.type);
 		}
-		return attribute;
+		return attribute as CDmxAttribute;
 	}
 
-	#parseArray(reader, pcf, type) {
+	#parseArray(reader: BinaryReader, pcf: SourcePCF, type: number): CDmxAttributeValue[] {
 		const valuesCount = reader.getUint32();
-		const value = [];
+		const value: CDmxAttributeValue[] = [];
 
 		for (let i = 0; i < valuesCount; ++i) {
 			value.push(this.#parseValue(reader, pcf, type));
@@ -136,11 +134,11 @@ export class SourceEnginePCFLoader extends SourceBinaryLoader {
 		return value;
 	}
 
-	#parseValue(reader, pcf, type) {
+	#parseValue(reader: BinaryReader, pcf: SourcePCF, type: number): CDmxAttributeValue {
 		let size = data_size[type];
-		let value = undefined;
+		let value: CDmxAttributeValue = undefined;
 
-		switch (type%14) {
+		switch (type % 14) {
 			case 1: // Element
 				value = this.getElement(pcf, reader.getInt32());
 				break;
@@ -158,7 +156,7 @@ export class SourceEnginePCFLoader extends SourceBinaryLoader {
 				break;
 			case 8: // Color
 				//value = {r:reader.getUint8(), g:reader.getUint8(), b:reader.getUint8(), a:reader.getUint8()};
-				value = new Color(reader.getUint8(),reader.getUint8(),reader.getUint8(),reader.getUint8());
+				value = new Color(reader.getUint8(), reader.getUint8(), reader.getUint8(), reader.getUint8());
 				//value = [reader.getUint8(), reader.getUint8(), reader.getUint8(), reader.getUint8()];
 				break;
 			case 9: // vec2
@@ -171,7 +169,7 @@ export class SourceEnginePCFLoader extends SourceBinaryLoader {
 				value = vec4.fromValues(reader.getFloat32(), reader.getFloat32(), reader.getFloat32(), reader.getFloat32());
 				break;
 			case 5:
-				if (pcf.binaryVersion<5) {
+				if (pcf.binaryVersion < 5) {
 					value = reader.getNullString();
 				} else {
 					value = this.getString(pcf, reader.getInt32())
@@ -179,17 +177,18 @@ export class SourceEnginePCFLoader extends SourceBinaryLoader {
 				break;
 			case 6:
 				size = reader.getInt32();
-				reader.seek(reader.tell()+size);
+				reader.seek(reader.tell() + size);
 				break;
 			default:
-				reader.seek(reader.tell() + size);
+				console.error('unknown type', type, 'in #parseValue', pcf);
+				throw 'fix me';
 				break;
 		}
 
 		return value;
 	}
 
-	getString(pcf, index) {
+	getString(pcf: SourcePCF, index: number): string {
 		if (pcf) {
 			const s = pcf.stringDict[index];
 			if (s) {
@@ -201,7 +200,7 @@ export class SourceEnginePCFLoader extends SourceBinaryLoader {
 		return '';
 	}
 
-	getElement(pcf, index) {
+	getElement(pcf: SourcePCF, index: number): CDmxElement | null {
 		if (pcf) {
 			const e = pcf.elementsDict[index];
 			if (e) {
@@ -216,14 +215,17 @@ export class SourceEnginePCFLoader extends SourceBinaryLoader {
 }
 registerLoader('SourceEnginePCFLoader', SourceEnginePCFLoader);
 
-class CDmxElement {
-	type;
-	name;
-	guid;
-	guid2;
+export type CDmxElement = {
+	type: string;
+	name: string;
+	guid: Uint8Array;
+	guid2: string;
+	attributes: CDmxAttribute[];
 }
-export class CDmxAttribute {
-	typeName;
-	type;
-	value;
+export type CDmxAttribute = {
+	typeName: string;
+	type: number;//TODO: create enum
+	value: CDmxAttributeValue | CDmxAttributeValue[];
 }
+
+export type CDmxAttributeValue = null | undefined | number | CDmxElement | Color | vec2 | vec3 | vec4 | string;
