d19b5e72ba63295d104326b3845ab00a9315b082
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-07-27 22:39:38 +0200

    PositionFromParentParticles: added param Inherited Velocity Scale

--- src/sourceengine/source1/particles/operators/initializers/positionfromparentparticles.ts
+++ src/sourceengine/source1/particles/operators/initializers/positionfromparentparticles.ts
@@ -1,32 +1,58 @@
 import { vec3 } from 'gl-matrix';
-
+import { CDmxAttributeValue } from '../../../export';
+import { SourceEngineParticle } from '../../particle';
 import { SourceEngineParticleOperators } from '../../sourceengineparticleoperators';
 import { SourceEngineParticleOperator } from '../operator';
 
+const positionFromParentParticlesTempVec3_0 = vec3.create();
+const positionFromParentParticlesTempVec3_1 = vec3.create();
+
+const DEFAULT_VELOCITY_SCALE = 0;/* TODO: check default value*/
+
 export class PositionFromParentParticles extends SourceEngineParticleOperator {
 	static functionName = 'Position From Parent Particles';
-	constructor() {
-		super();
+	#velocitySCale = DEFAULT_VELOCITY_SCALE;
+
+	paramChanged(name: string, param: CDmxAttributeValue | CDmxAttributeValue[]) {
+		console.info(name, param);
+		switch (name) {
+			case 'Inherited Velocity Scale':
+				this.#velocitySCale = param as number;//TODO: convert to number
+				break;
+			default:
+				super.paramChanged(name, param);
+				break;
+		}
 	}
 
-	doInit(particle, elapsedTime) {
+	doInit(particle: SourceEngineParticle, elapsedTime: number): void {
 		const parent = this.particleSystem.parentSystem;
-		if (parent == null) {
+		if (!parent) {
+			vec3.zero(particle.position);
+			vec3.zero(particle.prevPosition);
 			return;
 		}
+
+		const particleCount = parent.getActiveParticlesCount();
+		if (particleCount == 0) {
+			particle.die();
+		}
+
 		const base = parent.getParticle();
 		if (base != null) {
 			/*particle.timeToLive = 0;
 			particle.die();
 			return;*/
-			base.getWorldPos(particle.position);
+			//TODO: do subframe
+			//base.getWorldPos(particle.position);
 			base.getLocalPos(particle.position);
-			vec3.copy(particle.prevPosition, particle.position);
+			vec3.lerp(particle.prevPosition, base.position, base.prevPosition, this.#velocitySCale);
+			//vec3.copy(particle.prevPosition, particle.position);
 		} else {
 			particle.die();
 		}
 
-		particle.PositionFromParentParticles =true;
+		particle.PositionFromParentParticles = true;
 	}
 }
 SourceEngineParticleOperators.registerOperator(PositionFromParentParticles);
