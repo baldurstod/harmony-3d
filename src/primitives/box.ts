3734a4ddd4f586268455213d4c7463a8b20f32e3
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-09-01 17:12:56 +0200

    Box: reworked internals, added setSizeVec()

--- src/primitives/box.ts
+++ src/primitives/box.ts
@@ -1,22 +1,33 @@
-import { BoxBufferGeometry } from './geometries/boxbuffergeometry';
+import { vec3 } from 'gl-matrix';
+import { registerEntity } from '../entities/entities';
 import { JSONLoader } from '../importers/jsonloader';
+import { Material } from '../materials/material';
 import { MeshBasicMaterial } from '../materials/meshbasicmaterial';
 import { Mesh } from '../objects/mesh';
-import { registerEntity } from '../entities/entities';
-import { Material } from '../materials/material';
+import { BoxBufferGeometry } from './geometries/boxbuffergeometry';
+import { EntityParameters } from '../entities/entity';
+
+export type BoxParameters = EntityParameters & {
+	width?: number,
+	height?: number,
+	depth?: number,
+	widthSegments?: number,
+	heightSegments?: number,
+	depthSegments?: number,
+	material?: Material,
+};
 
 export class Box extends Mesh {
 	#widthSegments: number;
 	#heightSegments: number;
 	#depthSegments: number;
-	#width: number;
-	#height: number;
-	#depth: number;
-	constructor(params: any = {}) {
+	#size = vec3.create();// width, height, depth
+
+	constructor(params: BoxParameters = {}) {
 		super(new BoxBufferGeometry(), params.material ?? new MeshBasicMaterial());
-		this.#width = params.width ?? 1;
-		this.#height = params.height ?? this.#width;
-		this.#depth = params.depth ?? this.#width;
+		this.#size[0] = params.width ?? 1;
+		this.#size[1] = params.height ?? this.#size[0];
+		this.#size[2] = params.depth ?? this.#size[0];
 		this.#widthSegments = params.widthSegments ?? 1;
 		this.#heightSegments = params.heightSegments ?? 1;
 		this.#depthSegments = params.depthSegments ?? 1;
@@ -25,24 +36,24 @@ export class Box extends Mesh {
 	}
 
 	#updateGeometry() {
-		(this.geometry as BoxBufferGeometry).updateGeometry(this.#width, this.#height, this.#depth, this.#widthSegments, this.#heightSegments, this.#depthSegments);
+		(this.geometry as BoxBufferGeometry).updateGeometry(this.#size[0], this.#size[1], this.#size[2], this.#widthSegments, this.#heightSegments, this.#depthSegments);
 	}
 
 	buildContextMenu() {
 		return Object.assign(super.buildContextMenu(), {
 			Box_1: null,
-			width: { i18n: '#width', f: () => { const width = prompt('Width', String(this.#width)); if (width) { this.#width = Number(width); this.#updateGeometry(); } } },
-			height: { i18n: '#height', f: () => { const height = prompt('Height', String(this.#height)); if (height) { this.#height = Number(height); this.#updateGeometry(); } } },
-			depth: { i18n: '#depth', f: () => { const depth = prompt('Depth', String(this.#depth)); if (depth) { this.#depth = Number(depth); this.#updateGeometry(); } } },
-			cube: { i18n: '#cube', f: () => { let size: string | number = prompt('Cube size', '0'); if (size) { size = Number(size); this.#width = size; this.#height = size; this.#depth = size; this.#updateGeometry(); } } },
+			width: { i18n: '#width', f: () => { const width = prompt('Width', String(this.#size[0])); if (width) { this.#size[0] = Number(width); this.#updateGeometry(); } } },
+			height: { i18n: '#height', f: () => { const height = prompt('Height', String(this.#size[1])); if (height) { this.#size[1] = Number(height); this.#updateGeometry(); } } },
+			depth: { i18n: '#depth', f: () => { const depth = prompt('Depth', String(this.#size[2])); if (depth) { this.#size[2] = Number(depth); this.#updateGeometry(); } } },
+			cube: { i18n: '#cube', f: () => { let size: string | number = prompt('Cube size', '0'); if (size) { size = Number(size); this.#size[0] = size; this.#size[1] = size; this.#size[2] = size; this.#updateGeometry(); } } },
 		});
 	}
 
 	toJSON() {
 		const json = super.toJSON();
-		json.width = this.#width;
-		json.height = this.#height;
-		json.depth = this.#depth;
+		json.width = this.#size[0];
+		json.height = this.#size[1];
+		json.depth = this.#size[2];
 		json.widthSegments = this.#widthSegments;
 		json.heightSegments = this.#heightSegments;
 		json.depthSegments = this.#depthSegments;
@@ -66,24 +77,29 @@ export class Box extends Mesh {
 	}
 
 	setSize(width: number, height: number, depth: number) {
-		this.#width = width;
-		this.#height = height;
-		this.#depth = depth;
+		this.#size[0] = width;
+		this.#size[1] = height;
+		this.#size[2] = depth;
 		this.#updateGeometry();
 	}
 
-	setwidth(width: number) {
-		this.#width = width;
+	setSizeVec(size: vec3) {
+		vec3.copy(this.#size, size);
+		this.#updateGeometry();
+	}
+
+	setWidth(width: number) {
+		this.#size[0] = width;
 		this.#updateGeometry();
 	}
 
 	setHeight(height: number) {
-		this.#height = height;
+		this.#size[1] = height;
 		this.#updateGeometry();
 	}
 
 	setDepth(depth: number) {
-		this.#depth = depth;
+		this.#size[2] = depth;
 		this.#updateGeometry();
 	}
 }
