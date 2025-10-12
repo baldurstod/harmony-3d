87dced46d369346a5dbf5c985aa5cf41aabeff4d
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-10-12 22:23:14 +0200

    Updated eslint rules for floating promises

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,10 @@ export default tseslint.config(
       "@typescript-eslint/explicit-function-return-type": "error",
       "@typescript-eslint/no-deprecated": "error",
       "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/no-floating-promises": ["error", {
+        // No Promise in harmony-3d rejects
+        "allowForKnownSafePromises": [{ "from": "file", "name": "Promise" }]
+      }],
     }
   },
   {
