e12eb911924a2a6f5642a002b50bb67e672d7e20
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-12-06 17:44:48 +0100

    Moved meshbasic.wgsl

--- src/shaders/webgpu/shaders/export.ts
+++ src/shaders/webgpu/shaders/export.ts
@@ -1,4 +1,1 @@
-import { Shaders } from '../../shaders';
-
-import meshbasic_wgsl from './meshbasic.wgsl';
-Shaders['meshbasic.wgsl'] = meshbasic_wgsl;
+export * from './mesh/export';
---
+++ src/shaders/webgpu/shaders/mesh/export.ts
@@ -0,0 +1,4 @@
+import { Shaders } from '../../../shaders';
+
+import meshbasic_wgsl from './meshbasic.wgsl';
+Shaders['meshbasic.wgsl'] = meshbasic_wgsl;
