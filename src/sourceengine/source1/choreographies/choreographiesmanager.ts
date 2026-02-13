88dce3fa44aff8bea830e08f5e666a1b168fad1c
Guillaume <108530065+baldurstod@users.noreply.github.com>
2026-02-14 00:54:43 +0100

    Source 1: Fixed concurrent initialization of choreographies

--- src/sourceengine/source1/choreographies/choreographiesmanager.ts
+++ src/sourceengine/source1/choreographies/choreographiesmanager.ts
@@ -8,26 +8,30 @@ export class ChoreographiesManager {
 	static #playbackSpeed = 1.0;
 	static #playing = true;
 	static readonly #choreographies = new Set<Choreography>();
-	static #sceneImage?: Choreographies;
+	static #sceneImage?: Promise<Choreographies>;
 	static #vcds = new Map2<string, string, Choreography/*TODO: create a choreography definition*/>();
 
 	static async init(repositoryName: string, fileName: string): Promise<void> {
 		if (!this.#sceneImage) {
-			this.#sceneImage = new Choreographies();
-			await this.#sceneImage.loadFile(repositoryName, fileName);
-			GraphicsEvents.addEventListener(GraphicsEvent.Tick, (event: Event) => {
-				if ((event as CustomEvent<GraphicTickEvent>).detail.delta) {
-					this.step((event as CustomEvent<GraphicTickEvent>).detail.delta);
-				}
+			this.#sceneImage = new Promise<Choreographies>(resolve => {
+				const choreographies = new Choreographies();
+				choreographies.loadFile(repositoryName, fileName).then(() => resolve(choreographies));
+				GraphicsEvents.addEventListener(GraphicsEvent.Tick, (event: Event) => {
+					if ((event as CustomEvent<GraphicTickEvent>).detail.delta) {
+						this.step((event as CustomEvent<GraphicTickEvent>).detail.delta);
+					}
+				});
+
 			});
 		}
+		await this.#sceneImage;
 	}
 
 	static async playChoreography(repository: string, choreoName: string, actors: Source1ModelInstance[]): Promise<Choreography | null> {
 		let choreography: Choreography | undefined | null = this.#vcds.get(repository, choreoName);
 
 		if (!choreography && this.#sceneImage) {
-			choreography = await this.#sceneImage.getChoreography(choreoName);
+			choreography = await (await this.#sceneImage).getChoreography(choreoName);
 		}
 
 		if (!choreography) {
@@ -50,7 +54,7 @@ export class ChoreographiesManager {
 		}
 
 		if (this.#sceneImage) {
-			return await this.#sceneImage.getChoreography(choreoName);
+			return await (await this.#sceneImage).getChoreography(choreoName);
 		}
 		return null;
 	}
