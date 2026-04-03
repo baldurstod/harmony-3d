b934be72b74830d13402f73d3c74f3069162e70c
Guillaume <108530065+baldurstod@users.noreply.github.com>
2026-04-04 00:32:11 +0200

    Improved HasMaterials interface

--- src/interfaces/hasmaterials.ts
+++ src/interfaces/hasmaterials.ts
@@ -1,5 +1,5 @@
 export interface HasMaterials {
-	getSkins: () => Promise<Set<string>>;
-	getMaterialsName: (skin: string) => Promise<[string, Set<string>]>;
+	getSkins(): Promise<Set<string>>;
+	getMaterialsName(skin: string): Promise<[string, Set<string>]>;
 	setSkin(skin: string): Promise<void>;
 }
