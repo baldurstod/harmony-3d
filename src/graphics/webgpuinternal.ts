9074dcdddb539c525ad88ad16fac74129586f54c
Guillaume <108530065+baldurstod@users.noreply.github.com>
2026-04-18 12:27:31 +0200

    Added methods to track GPU buffers
    For debugging leaks

--- src/graphics/webgpuinternal.ts
+++ src/graphics/webgpuinternal.ts
@@ -1,3 +1,5 @@
+import { Millisecond } from 'harmony-types';
+
 export class WebGPUInternal {
 	static gpuContext: GPUCanvasContext;
 	static config: GPUCanvasConfiguration;
@@ -5,4 +7,41 @@ export class WebGPUInternal {
 	static device: GPUDevice;
 	static format: GPUTextureFormat;
 	static depthTexture: GPUTexture;
+}
+
+let buffers: Set<GPUBuffer>;
+
+export function trackGPUBuffers(): Set<GPUBuffer> {
+	if (buffers) {
+		return buffers;
+	}
+
+	buffers = new Set<GPUBuffer>();
+
+	const gpuDeviceOriginals = {
+		GPUDevice_createBuffer: GPUDevice.prototype.createBuffer,
+		GPUBuffer_destroy: GPUBuffer.prototype.destroy,
+	};
+
+	GPUDevice.prototype.createBuffer = function (descriptor: GPUBufferDescriptor): GPUBuffer {
+		const buffer = gpuDeviceOriginals.GPUDevice_createBuffer.call(this, descriptor);
+		buffers.add(buffer);
+		return buffer;
+	}
+
+	GPUBuffer.prototype.destroy = function (): undefined {
+		buffers.delete(this);
+		gpuDeviceOriginals.GPUBuffer_destroy.call(this);
+	}
+
+	return buffers;
+}
+
+export function logGPUBuffers(delay: Millisecond): ReturnType<typeof setInterval> {
+	return setInterval(() => {
+		let totalSize = 0;
+		buffers.forEach((buffer) => totalSize += buffer.size);
+
+		console.log(`GPU buffers: ${buffers.size}, total size: ${totalSize}`, buffers);
+	}, delay);
 }
